8a9a49ee0874146fb8c976f1f727b59972a87bf4
Ana Carolina D. C <76822093+devartes@users.noreply.github.com>
2023-11-09 08:05:58 -0300

    fix: add corrections to images and spacing

--- components/header/NavItem.tsx
+++ components/header/NavItem.tsx
@@ -29,7 +29,9 @@ function RenderLinks(links: Links[] | undefined, isChild = false) {
       {links.map((link) => (
         <ul>
           <a
-            class={`text-base ${isChild ? 'font-normal' : 'font-semibold'} leading-9 whitespace-nowrap`}
+            class={`text-base ${
+              isChild ? "font-normal" : "font-semibold"
+            } leading-9 whitespace-nowrap`}
             href={link.href}
           >
             {link.label}
@@ -49,9 +51,9 @@ function RenderImages(images: Image[] | undefined) {
   }
 
   return (
-    <div class="flex items-start gap-x-[30px] flex-wrap justify-center">
+    <div class="flex items-start gap-x-[30px] justify-center px-2">
       {images.map((imageData) => (
-        <figure class="flex flex-col items-center max-w-[242px] gap-y-[15px]">
+        <figure class="flex flex-col items-center w-[80.65%] gap-y-[15px]">
           <Image
             src={imageData.image}
             alt={imageData.alternateName}
@@ -73,11 +75,9 @@ function NavItem(item: NavItemProps) {
   return (
     <nav class="group flex items-center">
       <span class="group-hover:underline">{label}</span>
-      <div
-        class="hidden hover:flex group-hover:flex bg-base-100 z-50 items-start justify-center border-t border-b-2 border-base-200 w-screen flex-row-reverse pt-[55px] pb-14 top-full absolute left-0"
-      >
-        <div class="flex items-start gap-x-[140px] xl:flex-row flex-col-reverse">
-          <ul class="flex flex-1 items-start gap-x-[79px] mx-auto px-2">
+      <div class="hidden hover:flex group-hover:flex bg-base-100 z-50 items-start justify-center border-t border-b-2 border-base-200 w-screen flex-row-reverse pt-[55px] pb-14 top-full absolute left-0">
+        <div class="flex items-start gap-x-[141px] xl:flex-row flex-col-reverse">
+          <ul class="flex flex-1 items-start gap-x-[79px] px-2 w-full justify-between">
             {RenderLinks(links)}
           </ul>
           {RenderImages(images)}
