c5127da7763d2a14adaa78d713221e3d879ef2c4
Ana Carolina D. C <76822093+devartes@users.noreply.github.com>
2023-11-09 08:05:58 -0300

    fix: add corrections to images and spacing

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -26,7 +26,7 @@ function Navbar({ items, searchbar, logo }: {
       <div style={{ height: navbarHeight }} class="md:hidden flex flex-row justify-between items-center border-b border-base-200 w-full pl-2 pr-6 gap-2 relative">
         <MenuButton />
         {logo && (
-          <a href="/" class="flex-grow inline-flex items-center" style={{ minHeight: navbarHeight }} aria-label="Store logo">
+          <a href="/" class="flex-grow inline-flex items-center" aria-label="Store logo">
             <Image src={logo.src} alt={logo.alt} width={126} height={16} />
           </a>
         )}
