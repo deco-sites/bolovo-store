215dcc9bb2cbfc01db201f82aa041732a85a95f3
Martin Adam <martin.viana99@gmail.com>
2023-11-22 14:12:33 -0300

    Feat: Promotion Bar

--- components/header/Alert.tsx
+++ components/header/Alert.tsx
@@ -1,32 +1,58 @@
 import Slider from "$store/components/ui/Slider.tsx";
 import SliderJS from "$store/islands/SliderJS.tsx";
 import { useId } from "$store/sdk/useId.ts";
+import { Color } from "https://deno.land/x/color@v0.3.0/mod.ts";
 
-export interface Props {
-  alerts: string[];
+export interface barColor {
   /**
-   * @title Autoplay interval
-   * @description time (in seconds) to start the carousel autoplay
+   * @format color
+   * @title Background
+   * @default #121212
    */
-  interval?: number;
+  background: string;
+  /**
+   * @format color
+   * @title Text Color
+   * @default #FFFFFF
+   */
+  text: string;
 }
 
-function Alert({ alerts = [], interval = 5 }: Props) {
+export interface Props {
+  visible?: boolean; 
+  /** @format html */
+  text?: string;
+  layout?: barColor;
+  // interval?: number;
+  href?: string;
+}
+
+function Alert({visible = false, text = '', layout,  href = '/'}: Props) {
   const id = useId();
+
+  if (!visible) {
+    return null; 
+  }
+
+  const sliderStyle = {
+    backgroundColor: layout?.background || '#121212',
+  };
+  
+  const textStyle = {
+    color: layout?.text || '#FFFFFF',
+  };
 
   return (
     <div id={id}>
-      <Slider class="carousel carousel-center w-screen bg-secondary gap-6">
-        {alerts.map((alert, index) => (
-          <Slider.Item index={index} class="carousel-item">
-            <span class="text-sm text-secondary-content flex justify-center items-center w-screen h-[38px]">
-              {alert}
-            </span>
-          </Slider.Item>
-        ))}
+      <Slider style={sliderStyle} class={`carousel carousel-center w-screen gap-6`}>
+          <div class="carousel-item">
+            <a href={href} class="flex items-center">
+              <span style={textStyle} class="text-xs md:text-sm text-secondary-content flex justify-center items-center w-screen h-[36px]" dangerouslySetInnerHTML={{ __html: text }} />
+            </a>
+          </div>
       </Slider>
 
-      <SliderJS rootId={id} interval={interval && interval * 1e3} />
+      {/* <SliderJS rootId={id} interval={interval && interval * 1e3} /> */}
     </div>
   );
 }
