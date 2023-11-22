c2070b987cd514cee4c6ae848228956c04685a37
ApMatheus <matheusaparecido101@gmail.com>
2023-11-22 16:13:10 -0300

    feat: add fonts AcuminPro

--- components/GlobalTags.tsx
+++ components/GlobalTags.tsx
@@ -11,6 +11,56 @@ function GlobalTags() {
 
       {/* Web Manifest */}
       <link rel="manifest" href={asset("/site.webmanifest")} />
+
+      <style
+        type="text/css"
+        dangerouslySetInnerHTML={{
+          __html: `          
+          @font-face {
+            font-family: "AcuminPro-Regular";
+            src: url("${asset("/fonts/AcuminPro-Regular.woff2")}") format("woff2");
+            font-style: regular;
+            font-weight: normal;
+      },`,
+        }}
+      />
+      <style
+        type="text/css"
+        dangerouslySetInnerHTML={{
+          __html: `          
+          @font-face {
+            font-family: "AcuminPro-Bold";
+            src: url("${asset("/fonts/AcuminPro-Bold.woff2")}") format("woff2");
+            font-style: bold;
+            font-weight: normal;
+      },`,
+        }}
+      />
+      <style
+        type="text/css"
+        dangerouslySetInnerHTML={{
+          __html: `          
+          @font-face {
+            font-family: "AcuminPro-BoldItalic";
+            src: url("${asset("/fonts/AcuminPro-BoldItalic.woff2")}") format("woff2");
+            font-style: boldItalic;
+            font-weight: normal;
+      },`,
+        }}
+      />
+      <style
+        type="text/css"
+        dangerouslySetInnerHTML={{
+          __html: `          
+          @font-face {
+            font-family: "AcuminPro-Italic";
+            src: url("${asset("/fonts/AcuminPro-Italic.woff2")}") format("woff2");
+            font-style: italic;
+            font-weight: normal;
+      },`,
+        }}
+      />
+
     </Head>
   );
 }
