04bbcf5a8884c6fe4432d00e77c3f62d03abbdb9
Martin Adam <martin.viana99@gmail.com>
2023-11-24 11:30:34 -0300

    Fix: Responsividade

--- components/ui/Stores.tsx
+++ components/ui/Stores.tsx
@@ -30,17 +30,17 @@ export interface Props {
             <div className="container mx-auto py-10">
                 <div className={`flex flex-col ${containerClasses}`}>
                     {/* Imagem */}
-                    <div className="lg:w-1/2">
+                    <div className="mx-4 lg:mx-0 lg:w-1/2">
                         <img src={hero.media} alt={hero.altImg} className="object-cover w-full max-h-[577px]" />
                     </div>
 
                     {/* Título e Subtítulo */}
-                    <div className="lg:w-1/2 px-10 lg:px-0 lg:py-16">
-                        <div className={` bg-[#F6F6F6] ${alignment === 'right' ? 'lg:pr-20' : 'lg:pl-20'} md:h-full flex justify-center items-center feature-text-container relative py-10 lg:py-0`}>
+                    <div className="lg:w-1/2 px-11 lg:px-0 lg:py-16">
+                        <div className={` bg-[#F6F6F6] ${alignment === 'right' ? 'lg:pr-[24.4%]' : 'lg:pl-[24.4%]'} md:h-full flex justify-center items-center feature-text-container relative py-11 lg:py-0`}>
                             {/* Título e Subtítulo */}
-                            <div className={` flex flex-col max-w-[433px] ${alignment === 'right' ? 'lg:ml-auto' : 'lg:mr-auto'} gap-5`}>
-                                <h4 className="text-base font-bold">{content.storeName}</h4>
-                                <div className="text-base-300 leading-[26px]" dangerouslySetInnerHTML={{ __html: address ?? '' }} />
+                            <div className={`text-[13px] lg:text-[14px] xl:text-base leading-[20.8px] flex flex-col max-w-[433px] ${alignment === 'right' ? 'lg:ml-auto' : 'lg:mr-auto'} gap-5`}>
+                                <h4 className="text-[#121212] font-semibold">{content.storeName}</h4>
+                                <div className="lg:text-base-300" dangerouslySetInnerHTML={{ __html: address ?? '' }} />
                                 <ul>
                                 {content.socials?.map(({ label, text, href }, index) => (
                                         <li key={index}>
