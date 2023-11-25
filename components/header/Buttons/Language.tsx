6e730fc7bf20330ff5bd00d3169234a12ff5b697
ApMatheus <matheusaparecido101@gmail.com>
2023-11-25 11:12:27 -0300

    fix: code adjustments and variable types

--- components/header/Buttons/Language.tsx
+++ components/header/Buttons/Language.tsx
@@ -2,9 +2,10 @@ import Icon from "deco-sites/bolovo-store/components/ui/Icon.tsx";
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 import { usePartialSection } from "deco/hooks/usePartialSection.ts";
+import type { Country } from "../Header"
 
-export default function Lenguage({ countryFlag }: {
-    countryFlag: { countryLabel: string, countryImg: { img: ImageWidget, alt: string } }[];
+export default function LanguageSwitcher({ countryFlag }: {
+    countryFlag: Country[];
 }) {
     return (
         <>
@@ -19,11 +20,11 @@ export default function Lenguage({ countryFlag }: {
                         fill="none"
                     /></summary>
                 <ul className="p-2 shadow menu w-auto dropdown-content z-[1] bg-base-100 rounded-box">
-                    {countryFlag.map((iten, index) => {
+                    {countryFlag.map((iten) => {
                         return (
                             <button className=" cursor-pointer m-1 flex justify-center items-center uppercase gap-2 h-8 px-0 flex-row flex-nowrap border-none hover:border-none bg-transparent hover:bg-transparent w-max   text-base font-normal"
                                 {...usePartialSection({ props: { itenIndex: index } })}>
-                                <Image src={iten.countryImg.img} alt={iten.countryImg.alt} width={19} height={12} loading={"eager"}/>
+                                <Image src={iten.countryImg.img} alt={iten.countryImg.alt} width={19} height={12} loading={"eager"} />
                                 {iten.countryLabel}
                             </button>
                         )
--- components/header/NavItem.tsx
+++ components/header/NavItem.tsx
@@ -22,55 +22,59 @@ export interface Image {
   link?: string
 }
 
-function RenderLinks(links: Links[] | undefined, isChild = false) {
+function RenderLinks({ links }: Links[] | undefined, { isChild = false }: boolean) {
   if (!links || links.length === 0) {
     return null;
   }
 
   return (
-    <>
-      {links.map((link) => (
-        <ul>
-          <a
-            class={`text-base ${isChild ? "font-normal" : "font-semibold"
-              } leading-9 whitespace-nowrap hover:underline ease-in-out duration-200`}
-            href={link.href}
-          >
-            {link.label}
-          </a>
-          <li class="flex flex-col">
-            {RenderLinks(link.children, true)}
-          </li>
-        </ul>
-      ))}
-    </>
+    <div>
+      {links.map((link) => {
+        return (
+          <ul>
+            <a
+              class={`text-base ${isChild ? "font-normal" : "font-semibold"
+                } leading-9 whitespace-nowrap hover:underline ease-in-out duration-200`}
+              href={link.href}
+            >
+              {link.label}
+            </a>
+            <li class="flex flex-col">
+              <RenderLinks links={link.children} isChild={true} />
+            </li>
+          </ul>
+        )
+      })}
+    </div>
   );
 }
 
-function RenderImages(images: Image[] | undefined) {
+function RenderImages({ images }: Image[] | undefined) {
   if (!images || images.length === 0) {
     return null;
   }
 
   return (
     <div class="flex items-start px-2 justify-end w-2/4 flex-wrap">
-      {images.map((imageData) => (
-        <a href={imageData.link} class=" w-1/3 max-w-[180px] lg:max-w-[242px] hover:scale-105 ease-in duration-200">
-          <figure class="flex flex-col items-center w-full px-2 gap-y-[15px] ">
-            <Image
-              src={imageData.image}
-              alt={imageData.alternateName}
-              width={242}
-              height={242}
-              loading="lazy"
-              class="w-full "
-            />
-            <figcaption class="text-center text-base font-semibold leading-tight whitespace-normal">
-              {imageData.label}
-            </figcaption>
-          </figure>
-        </a>
-      ))}
+      {images.map((imageData) => {
+        return (
+          <a href={imageData.link} class=" w-1/3 max-w-[180px] lg:max-w-[242px] hover:scale-105 ease-in duration-200">
+            <figure class="flex flex-col items-center w-full px-2 gap-y-[15px] ">
+              <Image
+                src={imageData.image}
+                alt={imageData.alternateName}
+                width={242}
+                height={242}
+                loading="lazy"
+                class="w-full "
+              />
+              <figcaption class="text-center text-base font-semibold leading-tight whitespace-normal">
+                {imageData.label}
+              </figcaption>
+            </figure>
+          </a>
+        )
+      })}
     </div>
   );
 }
@@ -100,9 +104,9 @@ function NavItem(item: NavItemProps) {
       <div class="hidden group-hover:flex bg-base-100 z-50 items-start justify-between w-full border-t border-b-2 border-base-200 flex-row-reverse pt-[55px] pb-14 top-full absolute left-0">
         <div class="flex items-start justify-between flex-row w-full">
           <ul class="flex flex-1 items-start px-2 w-2/4 justify-start gap-8 flex-wrap xl:gap-16 2xl:gap-24">
-            {RenderLinks(links)}
+            <RenderLinks links={links} />
           </ul>
-          {RenderImages(images)}
+          <RenderImages images={images} />
         </div>
       </div>
     </nav>
