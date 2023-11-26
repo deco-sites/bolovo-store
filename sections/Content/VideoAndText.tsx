8a099233d995c45a3a334fbd711722b5ed9bc604
ApMatheus <matheusaparecido101@gmail.com>
2023-11-26 09:02:54 -0300

    Interface adjustments and image loading

--- sections/Content/VideoAndText.tsx
+++ sections/Content/VideoAndText.tsx
@@ -1,6 +1,10 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 
+interface Images {
+    src: ImageWidget;
+    alt: string;
+}
 
 export interface Props {
     /**
@@ -25,33 +29,29 @@ export interface Props {
     /**
      * @title List of imagens
      */
-    listImages: {
-        src: ImageWidget;
-        alt: string;
-    }[];
-
+    listImages: Images[]
     /**
      * @title Link of video 
-     * @description Infos of secord column
+     * @description Infos of second column
      */
     videoLink: string;
 
 }
 
-export default function TextPlusVideo({ directionInverse, image, textContent, listImages, videoLink }: Props) {
+export default function VideoAndText({ directionInverse, image, textContent, listImages, videoLink }: Props) {
     return (
         <div class="flex flex-row px-2 w-full py-8 2xl:py-16">
             <div style={{ flexDirection: directionInverse ? "row-reverse" : "row" }} class="flex flex-row justify-center items-center gap-4 w-full">
                 <div class="flex flex-col justify-between h-full items-center w-2/4 gap-3 2xl:gap-20">
-                    <Image src={image.src} alt={image.alt} width={350} height={69} loading={"eager"} class="max-w-[200px] md:max-w-[220px] xl:max-w-[350px] 2xl:max-w-[400px]" />
-                    <div dangerouslySetInnerHTML={{ __html: textContent }} class=" font-AcuminPro-Regular text-[10px] leading-4 lg:text-[13px] lg:leading-5 xl:text-sm xl:leading-6 2xl:text-base 2xl:leading-7">
+                    <Image src={image.src} alt={image.alt} width={350} height={69} loading={"lazy"} class="max-w-[200px] md:max-w-[220px] xl:max-w-[350px] 2xl:max-w-[400px]" />
+                    <div dangerouslySetInnerHTML={{ __html: textContent }} class=" font-acumin font-normal text-[10px] leading-4 lg:text-[13px] lg:leading-5 xl:text-sm xl:leading-6 2xl:text-base 2xl:leading-7">
 
                     </div>
                     <div class="flex flex-row gap-2">
                         {listImages.map((img) => {
                             return (
                                 <div>
-                                    <Image src={img.src} alt={img.alt} width={100} height={37} loading={"eager"} class="max-w-[70px] xl:max-w-[100px] 2xl:max-w-[150px]" />
+                                    <Image src={img.src} alt={img.alt} width={100} height={37} loading={"lazy"} class="max-w-[70px] xl:max-w-[100px] 2xl:max-w-[150px]" />
                                 </div>
                             )
                         })}
