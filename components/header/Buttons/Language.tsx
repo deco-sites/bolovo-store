c0c19eb547c3ca982644cad456d5fa0a916f5bfb
soutofernando <fernandosoutol45@gmail.com>
2023-12-14 01:21:13 -0300

    Header mobile

--- components/header/Buttons/Language.tsx
+++ components/header/Buttons/Language.tsx
@@ -8,15 +8,18 @@ export default function LanguageSwitcher({ countryFlag }: {
 }) {
   return (
     <>
-      <details className="dropdown px-0">
-        <summary className=" btn px-2 flex flex-row flex-nowrap border-none hover:border-none bg-transparent hover:bg-transparent w-max uppercase text-base font-normal">
-          <Image
-            src={countryFlag[0].countryImg.img}
-            alt={countryFlag[0].countryImg.alt}
-            width={19}
-            height={12}
-          />
-          {countryFlag[0].countryLabel}
+      <details class="dropdown px-0 w-full md:w-auto">
+        <summary class=" btn px-0 md:px-2 flex flex-row flex-grow md:flex-nowrap border-none hover:border-none justify-between md:justify-normal bg-transparent hover:bg-transparent w-full uppercase text-base font-normal">
+          <div class="flex flex-row items-center">
+            <Image
+              src={countryFlag[0].countryImg.img}
+              alt={countryFlag[0].countryImg.alt}
+              width={19}
+              height={12}
+              class="w-[19px] h-3 mr-1"
+            />
+            <span>{countryFlag[0].countryLabel}</span>
+          </div>
           <Icon
             id="ChevronDown"
             size={11}
@@ -24,14 +27,12 @@ export default function LanguageSwitcher({ countryFlag }: {
             fill="none"
           />
         </summary>
-        <ul className="p-2 shadow menu w-auto dropdown-content z-[1] bg-base-100 rounded-box">
+        <ul class="p-2 shadow menu w-auto dropdown-content z-[1] bg-base-100 rounded-box">
           {countryFlag.map((iten) => {
             return (
-              <button
-                className=" cursor-pointer m-1 flex justify-center items-center uppercase gap-2 h-8 px-0 flex-row flex-nowrap border-none hover:border-none bg-transparent hover:bg-transparent w-max   text-base font-normal"
-                {...usePartialSection({
-                  props: { Abbreviation: iten.languageAbbreviation },
-                })}
+              <button class="cursor-pointer m-1 flex justify-center items-center uppercase gap-2 h-8 px-0 flex-row flex-nowrap border-none hover:border-none bg-transparent hover:bg-transparent w-max text-base font-normal" // {...usePartialSection({
+                //   props: { Abbreviation: iten.languageAbbreviation },
+                // })}
               >
                 <Image
                   src={iten.countryImg.img}
