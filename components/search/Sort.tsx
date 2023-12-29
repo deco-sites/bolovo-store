9929990e2c0e4b4b18864d2d4fb35387a80b19d5
soutofernando <fernandosoutol45@gmail.com>
2023-12-29 15:04:41 -0300

    Remove container and add hover

--- components/search/Sort.tsx
+++ components/search/Sort.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "preact/hooks";
 import SortMenu from "./SortMenu.tsx";
 import { ProductListingPage } from "apps/commerce/types.ts";
 import Icon from "$store/components/ui/Icon.tsx"
+import Button from "deco-sites/bolovo-store/components/ui/Button.tsx";
 
 export type Props = Pick<ProductListingPage, "sortOptions">;
 
@@ -30,22 +31,22 @@ function Sort({ sortOptions }: Props) {
 
   return (
     <div class="relative z-20 w-full sm:w-auto" ref={sortContainerRef}>
-      <button
-        class={`appearance-none py-[5px] w-full bg-white border-b-1 border-gray-light-footer focus:outline-none outline-none transition duration-150 ease-in-out flex justify-between items-center ${
-          isOpen ? "border border-black rounded-t-lg" : ""
+      <Button
+        class={`btn-ghost btn-xs text-[13px] top-1/2 px-2 py-[5px] px- font-light uppercase leading-0 rounded-[20px] hover:bg-transparent ${
+          isOpen ? "border border-black rounded-t-lg" : "hover:border hover:border-black"
         }`}
         onClick={() => setIsOpen(!isOpen)}
         aria-haspopup="listbox"
         aria-expanded={isOpen}
       >
         <span class="text-[13px] leading-0 flex px-[11px]">
-          <span class="w-[19px] h-[10px] flex flex-col mr-[6px] mt-1">
+          <span class="w-[19px] h-[10px] flex flex-col mr-[6px]">
             <Icon id="FilterArrowLeft" />
             <Icon id="FilterArrowRight" />
           </span>
           ORDENAR
         </span>
-      </button>
+      </Button>
       {isOpen && <SortMenu sortOptions={sortOptions} />}
     </div>
   );
