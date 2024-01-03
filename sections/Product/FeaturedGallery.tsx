b8a3c940d31717e11191e143a11b963b317e5208
Martin Adam <martin.viana99@gmail.com>
2024-01-03 16:25:18 -0300

    Ajustes ProductCard e ProductCardGallery

--- sections/Product/FeaturedGallery.tsx
+++ sections/Product/FeaturedGallery.tsx
@@ -1,7 +1,7 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 import type { Product } from "apps/commerce/types.ts";
-import ProductCard from "../../components/product/ProductCard.tsx";
+import ProductCardGallery from "../../components/content/ProductCardGallery.tsx";
 export interface Props {
     title: string;
     product: Product[] | null;
@@ -38,22 +38,21 @@ export default function FeaturedGallery({ title, featuredPhoto, contentDirection
     }
 
     return (
-        <div class="flex px-4 flex-col gap-6 py-8">
+        <div class="flex px-[15px] flex-col gap-6 py-8">
             <h2 class=" text-base text-left uppercase font-bold">
                 {title}
             </h2>
-            <div class={`flex gap-2 lg:gap-4  
+            <div class={`flex gap-2 lg:gap-[15px]  
                 ${MOBILE_DIRECTION[contentDirection?.mobile ?? "imagem acima"]
                 } ${DESKTOP_DIRECTION[contentDirection?.desktop ?? "imagem na direita"]} `}>
-                <a href={featuredPhoto.href} class=" w-full lg:w-[55%] cursor-pointer">
-                    <Image src={featuredPhoto.src} alt={featuredPhoto.alt} width={400} height={400} loading={"lazy"} class="w-full " />
+                <a href={featuredPhoto.href} class="flex w-full lg:w-[55.15%] cursor-pointer">
+                    <Image src={featuredPhoto.src} alt={featuredPhoto.alt} width={400} height={400} loading={"lazy"} class="aspect-square w-full lg:pb-[5px]" />
                 </a>
-                <div class="w-full lg:w-[45%] min-h-[400px]">
-                    <ProductCard
+                <div class="flex w-full lg:w-[44.08%] min-h-[400px]">
+                    <ProductCardGallery
                         product={product[0]}
                     />
                 </div>
-
             </div>
         </div>
     )
