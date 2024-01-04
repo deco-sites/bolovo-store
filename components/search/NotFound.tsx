127c1b9f7add449a054921b07333a861a3a7ea76
ApMatheus <matheusaparecido101@gmail.com>
2024-01-04 10:11:40 -0300

    fix: adjustments to productShelf and background image

--- components/search/NotFound.tsx
+++ components/search/NotFound.tsx
@@ -43,7 +43,7 @@ const POSITION_STEPS = {
 }
 
 function Container({ props }: { props: PropsNotFound }) {
-    const { title, imagem, stepsBySteps, theme, positionSteps, shelfs } = props
+    const { title, imagem, stepsBySteps, theme, positionSteps} = props;
 
     return (
         <div class="w-full flex justify-center items-center pt-12 pb-28 px-4 flex-col gap-7 ">
@@ -63,7 +63,7 @@ function Container({ props }: { props: PropsNotFound }) {
             <p class={`uppercase font-bold text-center ${TEXT_COLOR[theme ?? "dark"]}`}>
                 {title}
             </p>
-            <ul class={`flex w-full flex-col gap-4 pt-8 lg:pt-16 pb-24 justify-evenly ${POSITION_STEPS[positionSteps ?? "horizontal"]}`}>
+            <ul class={`flex w-full flex-col gap-4 pt-8 lg:pt-16 pb-12 justify-evenly ${POSITION_STEPS[positionSteps ?? "horizontal"]}`}>
                 {stepsBySteps.map((step, index) => (
                     <li class={`flex flex-col ${POSITION_STEPS[positionSteps ?? "horizontal"]}`}>
                         <span class={`font-averia font-bold text-5xl italic text-center lg:text-start ${TEXT_COLOR[theme ?? "dark"]}`}>{index + 1}.</span>
@@ -72,8 +72,6 @@ function Container({ props }: { props: PropsNotFound }) {
                     </li>
                 ))}
             </ul>
-            <ProductShelf {...shelfs.primaryShelf} />
-            <ProductShelf {...shelfs.secondShelf} />
         </div>
     );
 
@@ -82,43 +80,66 @@ function Container({ props }: { props: PropsNotFound }) {
 
 export default function NotFound({ props }: { props: PropsNotFound }) {
 
-    const { backgroundImage, } = props
+    const { backgroundImage, shelfs } = props
+    const { primaryShelf, secondShelf } = shelfs;
 
     if (backgroundImage?.active) {
         return (
-            <div class="relative w-full h-full">
-                <Picture preload={false}>
-                    <Source
-                        media="(max-width: 1023px)"
-                        fetchPriority={"auto"}
-                        src={backgroundImage.mobile}
-                        width={320}
-                        height={465}
-                    />
-                    {/* add  souce mobile e desk */}
-                    <Source
-                        media="(min-width: 1024px)"
-                        fetchPriority={"auto"}
-                        src={backgroundImage.desktop}
-                        width={1024}
-                        height={505}
-                    />
-                    <img
-                        class="w-full h-full absolute top-0 left-0 object-cover"
-                        loading={"lazy"}
-                        src={backgroundImage.desktop}
-                        alt={backgroundImage.alt}
-                    />
-                </Picture>
-                <div class="relative w-full h-ful bg-[#0000002e]">
-                    <Container props={props} />
+            <div class="flex flex-col w-full h-full">
+                <div class="relative w-full h-full">
+                    <Picture preload={false}>
+                        <Source
+                            media="(max-width: 641px)"
+                            fetchPriority={"auto"}
+                            src={backgroundImage.mobile}
+                            width={375}
+                            height={400}
+                        />
+                        <Source
+                            media="(min-width: 642px and max-width: 1023px)"
+                            fetchPriority={"auto"}
+                            src={backgroundImage.mobile}
+                            width={1023}
+                            height={465}
+                        />
+                        <Source
+                            media="(min-width: 1024px and max-width: 1365px)"
+                            fetchPriority={"auto"}
+                            src={backgroundImage.desktop}
+                            width={1024}
+                            height={465}
+                        />
+                        <Source
+                            media="(min-width: 1366)"
+                            fetchPriority={"auto"}
+                            src={backgroundImage.desktop}
+                            width={1366}
+                            height={610}
+                        />
+                        <img
+                            class="w-full h-full absolute top-0 left-0 object-cover"
+                            loading={"lazy"}
+                            src={backgroundImage.mobile}
+                            alt={backgroundImage.alt}
+                        />
+                    </Picture>
+                    <div class="relative w-full h-ful bg-[#0000002e]">
+                        <Container props={props} />
+                    </div>
                 </div>
+                <ProductShelf products={primaryShelf.products} title={primaryShelf.title} layout={primaryShelf.layout} cardLayout={primaryShelf.cardLayout} seeMore={primaryShelf.seeMore} />
+                <ProductShelf products={secondShelf.products} title={secondShelf.title} layout={secondShelf.layout} cardLayout={secondShelf.cardLayout} seeMore={secondShelf.seeMore} />
             </div>
+
         )
     }
 
     return (
-        <Container props={props} />
+        <div class="flex flex-col w-full h-full">
+            <Container props={props} />
+            <ProductShelf products={primaryShelf.products} title={primaryShelf.title} layout={primaryShelf.layout} cardLayout={primaryShelf.cardLayout} seeMore={primaryShelf.seeMore} />
+            <ProductShelf products={secondShelf.products} title={secondShelf.title} layout={secondShelf.layout} cardLayout={secondShelf.cardLayout} seeMore={secondShelf.seeMore} />
+        </div>
     )
 
 
