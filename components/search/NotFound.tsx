e2fe4853b54607262605a163a78ac25ccdeb244b
ApMatheus <matheusaparecido101@gmail.com>
2024-01-06 12:48:10 -0300

    fix: adjustments reference in variant layout

--- components/search/NotFound.tsx
+++ components/search/NotFound.tsx
@@ -7,7 +7,8 @@ import type { Props } from "$store/components/product/ProductShelf.tsx"
 
 export interface PropsNotFound {
     title: string;
-    imagem: {
+    imagem?: {
+        active?: boolean;
         src: ImageWidget;
         alt: string;
     };
@@ -39,40 +40,41 @@ export const TEXT_COLOR = {
 
 const POSITION_STEPS = {
     "horizontal": "lg:flex-row",
-    "vertical": "lg:flex-col items-center",
+    "vertical": "lg:flex-col",
 }
 
 function Container({ props }: { props: PropsNotFound }) {
-    const { title, imagem, stepsBySteps, theme, positionSteps} = props;
+    const { title, imagem, stepsBySteps, theme, positionSteps } = props;
 
     return (
-        <div class="w-full flex justify-center items-center pt-12 pb-28 px-4 flex-col gap-7 ">
-            <Image
-                loading="lazy"
-                width={106}
-                height={106}
-                src={imagem.src}
-                alt={imagem.alt}
-                class=" animate-spin-slow"
-            >
-            </Image>
-            <div>
+        <div class="w-full flex justify-center items-center pt-12 pb-28 lg:pb-44 px-4 flex-col gap-7 ">
+            {imagem?.active &&
+                <Image
+                    loading="lazy"
+                    width={106}
+                    height={106}
+                    src={imagem.src}
+                    alt={imagem.alt}
+                    class=" animate-spin-slow"
+                >
+                </Image>
+            }
+            <div class={`${!imagem?.active && "mt-24"}`}>
                 <SearchedWord theme={theme} />
-
             </div>
             <p class={`uppercase font-bold text-center ${TEXT_COLOR[theme ?? "dark"]}`}>
                 {title}
             </p>
-            <ul class={`flex w-full flex-col gap-4 pt-8 lg:pt-16 pb-12 justify-evenly ${POSITION_STEPS[positionSteps ?? "horizontal"]}`}>
+            <ul class={`flex w-full flex-col gap-4 pt-4 lg:pt-8 justify-evenly px-2 ${POSITION_STEPS[positionSteps ?? "horizontal"]}`}>
                 {stepsBySteps.map((step, index) => (
-                    <li class={`flex flex-col ${POSITION_STEPS[positionSteps ?? "horizontal"]}`}>
-                        <span class={`font-averia font-bold text-5xl italic text-center lg:text-start ${TEXT_COLOR[theme ?? "dark"]}`}>{index + 1}.</span>
-                        <span class={`uppercase  text-center flex justify-center items-end flex-wrap ${TEXT_COLOR[theme ?? "dark"]}`}
+                    <li class={`flex flex-col gap-1 items-center lg:min-w-[33%] justify-center ${POSITION_STEPS[positionSteps ?? "horizontal"]}`}>
+                        <span class={`font-averia font-bold  text-4xl italic text-center lg:text-start ${TEXT_COLOR[theme ?? "dark"]}`}>{index + 1}.</span>
+                        <span class={`uppercase  text-center text-sm flex justify-center items-end flex-wrap ${TEXT_COLOR[theme ?? "dark"]}`}
                             dangerouslySetInnerHTML={{ __html: step.richeText }} ></span>
                     </li>
                 ))}
             </ul>
-        </div>
+        </div >
     );
 
 }
@@ -92,38 +94,48 @@ export default function NotFound({ props }: { props: PropsNotFound }) {
                             media="(max-width: 641px)"
                             fetchPriority={"auto"}
                             src={backgroundImage.mobile}
-                            width={375}
-                            height={400}
+                            width={431}
+                            height={431}
                         />
                         <Source
-                            media="(min-width: 642px and max-width: 1023px)"
+                            media="(min-width: 641px) and (max-width: 1023px)"
                             fetchPriority={"auto"}
                             src={backgroundImage.mobile}
-                            width={1023}
-                            height={465}
+                            width={641}
+                            height={641}
                         />
                         <Source
-                            media="(min-width: 1024px and max-width: 1365px)"
+                            media="(min-width: 1024px) and (max-width: 1365px)"
                             fetchPriority={"auto"}
                             src={backgroundImage.desktop}
                             width={1024}
-                            height={465}
+                            height={412}
                         />
                         <Source
-                            media="(min-width: 1366)"
+                            media="(min-width: 1366px) and (max-width: 1499px)"
                             fetchPriority={"auto"}
                             src={backgroundImage.desktop}
                             width={1366}
-                            height={610}
+                            height={550}
+                            decoding="async"
+                        />
+                        <Source
+                            media="(min-width: 1500px)"
+                            fetchPriority={"auto"}
+                            src={backgroundImage.desktop}
+                            width={1500}
+                            height={604}
+                            decoding="async"
                         />
                         <img
-                            class="w-full h-full absolute top-0 left-0 object-cover"
+                            className="w-full h-full absolute top-0 left-0 object-cover"
                             loading={"lazy"}
                             src={backgroundImage.mobile}
                             alt={backgroundImage.alt}
                         />
                     </Picture>
-                    <div class="relative w-full h-ful bg-[#0000002e]">
+
+                    <div class="relative w-full h-ful bg-[#17171799]">
                         <Container props={props} />
                     </div>
                 </div>
