14a37cd059d45e3f7764141d3b0783fdfe538586
ApMatheus <matheusaparecido101@gmail.com>
2024-01-15 08:59:28 -0300

    fix: not reload in sku selector

---
+++ sdk/url.ts
@@ -0,0 +1,6 @@
+export const relative = (link?: string | undefined) => {
+    const linkUrl = link ? new URL(link) : undefined;
+    const linkPath = linkUrl ? `${linkUrl.pathname}${linkUrl.search}` : undefined;
+    return linkPath;
+  };
+  
