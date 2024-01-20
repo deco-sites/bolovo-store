bcd8d3f9065bae6a1f5ad6955727372ed5bed43e
ApMatheus <matheusaparecido101@gmail.com>
2024-01-20 18:07:03 -0300

    feat: updates in Banner Category section

--- components/ui/CategoryBanner.tsx
+++ components/ui/CategoryBanner.tsx
@@ -10,7 +10,10 @@ export interface Banner {
   matcher: string;
   /** @description text to be rendered on top of the image */
   title?: string;
-  /** @description text to be rendered on top of the image */
+  /** 
+   * @description text to be rendered on top of the image 
+   * @format html
+  */
   subtitle?: string;
   image: {
     /** @description Image for big screens */
@@ -19,6 +22,7 @@ export interface Banner {
     mobile: ImageWidget;
     /** @description image alt text */
     alt?: string;
+    href?: string;
   };
 }
 
@@ -46,39 +50,56 @@ function Banner(props: SectionProps<ReturnType<typeof loader>>) {
     return null;
   }
 
-  const { title, subtitle, image } = banner;
+  const { title, subtitle, image, href } = banner;
 
   return (
-    <div class="grid grid-cols-1 grid-rows-1">
+    <a href={href} class="grid grid-cols-1 grid-rows-1">
       <Picture preload class="col-start-1 col-span-1 row-start-1 row-span-1">
         <Source
+          media="(max-width: 641px)"
+          fetchPriority={"high"}
           src={image.mobile}
-          width={360}
-          height={120}
-          media="(max-width: 767px)"
+          width={200}
+          height={253}
         />
         <Source
-          src={image.desktop}
-          width={1440}
-          height={200}
-          media="(min-width: 767px)"
+          media="(min-width: 641px) and (max-width: 1023px)"
+          fetchPriority={"high"}
+          src={image.mobile}
+          width={500}
+          height={634}
         />
-        <img class="w-full" src={image.desktop} alt={image.alt ?? title} />
+        <Source
+          media="(min-width: 1024px) and (max-width: 1499px)"
+          fetchPriority={"high"}
+          src={image.desktop}
+          width={1000}
+          height={309}
+        />
+        <Source
+          media="(min-width: 1500px)"
+          fetchPriority={"high"}
+          src={image.desktop}
+          width={1500}
+          height={463}
+        />
+        <img class="w-full"
+          loading="eager"
+          decoding="auto"
+          src={image.desktop}
+          alt={image.alt ?? title}
+          width={430}
+          height={548} />
       </Picture>
 
-      <div class="container flex flex-col items-center justify-center sm:items-start col-start-1 col-span-1 row-start-1 row-span-1 w-full">
-        <h1>
-          <span class="text-5xl font-medium text-base-100">
-            {title}
-          </span>
-        </h1>
-        <h2>
-          <span class="text-xl font-medium text-base-100">
-            {subtitle}
-          </span>
-        </h2>
-      </div>
-    </div>
+      {(title || subtitle) && < div class=" flex flex-col py-5 px-4 lg:py-9 lg:px-6 items-start justify-end sm:items-start col-start-1 col-span-1 row-start-1 row-span-1 w-full gap-3">
+        {title && <h1 class=" text-6xl lg:text-[5rem] font-normal text-base-100 font-eb-garamond">
+          {title}
+        </h1>}
+        {subtitle && <span class="text-base-100 text-xs lg:text-base" dangerouslySetInnerHTML={{ __html: subtitle }}>
+        </span>}
+      </div>}
+    </a >
   );
 }
 
