c3e483e54a710b66ea7c024629407dc30ddb7a8f
ApMatheus <matheusaparecido101@gmail.com>
2024-02-02 22:41:57 -0300

    fix: possible error in variants

--- components/product/QuickShop/SkuSelector.tsx
+++ components/product/QuickShop/SkuSelector.tsx
@@ -13,27 +13,32 @@ interface Props {
 export default function SkuSelector(
   { variants, productID, availability, additionalProperty, onAddItem }: Props,
 ) {
+  if (
+    variants === undefined || variants === null || variants?.length == 0
+  ) {
+    return null;
+  }
+
   return (
     <>
-      {variants && variants.length !== 1
-        ? variants?.map((variant) => (
+      {variants.length !== 1
+        ? variants.map((variant) => (
           <li
             class={`ease-in-out duration-500 delay-300`}
           >
             {variant.inStock
               ? (
                 <button
-                  onClick={() =>
-                    onAddItem(variant.id ?? "", additionalProperty)}
+                  onClick={() => onAddItem(variant.id, additionalProperty)}
                   class="hover:font-semibold"
                 >
-                  {variant?.size?.substring(2, 0)}
+                  {variant.size?.substring(2, 0)}
                 </button>
               )
               : (
                 <button class="text-[#E0E0E0] cursor-not-allowed relative false w-auto flex items-center justify-center group/number ">
                   <span class="cursor-not-allowed false flex h-6 w-6 text-center items-center justify-center">
-                    {variant?.size?.substring(2, 0)}
+                    {variant.size?.substring(2, 0)}
                   </span>
                   <span class="false absolute border-b border-[#E0E0E0] rotate-[-45deg] w-[24px]">
                   </span>
