75a724d08d2c4b17d07dd06d63389701db33e638
soutofernando <fernandosoutol45@gmail.com>
2024-02-04 00:48:55 -0300

    Some adjustments

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,6 +1,5 @@
 import BackToTop from "$store/components/footer/BackToTop.tsx";
 import ColorClasses from "$store/components/footer/ColorClasses.tsx";
-import Divider from "$store/components/footer/Divider.tsx";
 import ExtraLinks from "$store/components/footer/ExtraLinks.tsx";
 import FooterItems from "$store/components/footer/FooterItems.tsx";
 import Logo from "$store/components/footer/Logo.tsx";
@@ -10,7 +9,6 @@ import RegionSelector from "$store/components/footer/RegionSelector.tsx";
 import Social from "$store/components/footer/Social.tsx";
 import Newsletter from "$store/islands/Newsletter.tsx";
 import type { ImageWidget } from "apps/admin/widgets.ts";
-import PoweredByDeco from "apps/website/components/PoweredByDeco.tsx";
 import LanguageSwitcher from "$store/components/header/Buttons/Language.tsx";
 import { Country } from "$store/components/header/Header.tsx";
 import type { HTMLWidget } from "apps/admin/widgets.ts";
@@ -93,12 +91,6 @@ export interface Layout {
     | "Accent"
     | "Base 100"
     | "Base 100 inverted";
-  variation?:
-    | "Variation 1"
-    | "Variation 2"
-    | "Variation 3"
-    | "Variation 4"
-    | "Variation 5";
   hide?: {
     logo?: boolean;
     newsletter?: boolean;
@@ -222,7 +214,6 @@ function Footer({
     "© BOLOVO | VNDA - TECNOLOGIA EM ECOMMERCE | CNPJ: 11625557/0001-34",
   layout = {
     backgroundColor: "Primary",
-    variation: "Variation 1",
     hide: {
       logo: false,
       newsletter: false,
@@ -240,23 +231,18 @@ function Footer({
   const _newsletter = layout?.hide?.newsletter ? <></> : (
     <Newsletter
       content={newsletter}
-      layout={{
-        tiled: layout?.variation == "Variation 4" ||
-          layout?.variation == "Variation 5",
-      }}
     />
   );
   const _sectionLinks = layout?.hide?.sectionLinks ? <></> : (
     <FooterItems
       whatsApp={whatsApp}
       sections={sections}
-      justify={layout?.variation == "Variation 2" ||
-        layout?.variation == "Variation 3"}
+      justify={false}
     />
   );
   const _social = layout?.hide?.socialLinks
     ? <></>
-    : <Social content={social} vertical={layout?.variation == "Variation 3"} />;
+    : <Social content={social} />;
   const _payments = layout?.hide?.paymentMethods
     ? <></>
     : <PaymentMethods content={payments} />;
@@ -277,13 +263,10 @@ function Footer({
       }`}
     >
       <div class="w-full">
-        {(!layout?.variation || layout?.variation == "Variation 1") && (
           <div>
-            <Divider class="border-primary" />
-            <div class="flex flex-row justify-center pt-[45px] pb-[51px] md:pt-[65px] md:pb-[71px] md:bg-transparent bg-[#F6F6F6]">
+            <div class="flex flex-row justify-center pt-[45px] pb-[51px] md:pt-[65px] md:pb-[71px] md:bg-transparent bg-[#F6F6F6] border-y border-primary">
               {_newsletter}
             </div>
-            <Divider class="border-primary" />
             <div class="flex flex-col w-full flex-grow gap-5">
               <div class="w-full flex flex-row items-center justify-between pl-[29px] pr-[25px] pt-[57px] md:pt-[45px]">
                 {footerLogos?.map((logo) => (
@@ -307,12 +290,12 @@ function Footer({
                   {footerTexts?.subTitle}
                 </p>
                 <div
-                  class="md:mx-auto px-4 md:px-0 md:max-w-[512px] w-full font-normal text-xs leading-[22.8px] tracking-[2%]"
+                  class="md:mx-auto px-4 md:px-0 md:max-w-[512px] w-full font-normal text-xs leading-[22px] tracking-[2%]"
                   dangerouslySetInnerHTML={{ __html: footerTexts?.text ?? "" }}
                 />
               </div>
               <div class="md:hidden flex flex-row items-center justify-center mb-[34px]">
-                <span class="font-medium leading-[22.4px] text-sm uppercase text-primary mr-[11px]">
+                <span class="font-medium leading-[22px] text-sm uppercase text-primary mr-[11px]">
                   {changeCountryText}
                 </span>
                 <LanguageSwitcher
@@ -329,24 +312,24 @@ function Footer({
               <div class="border-b ml-7 mr-[25px]" />
               <div class="flex flex-row justify-between items-center md:pl-[29px] md:pr-[25px] px-[15px] md:px-0 w-full">
                 <a
-                  href={`https://api.whatsapp.com/send?phone=55${whatsApp.whatsAppNumber}&text=${whatsApp.whatsAppText}`}
+                  href={`https://api.whatsapp.com/send?phone=${whatsApp.whatsAppNumber}&text=${whatsApp.whatsAppText}`}
                   class="md:hidden flex flex-row rounded-[19px] border border-primary items-center cursor-pointer h-[38px] w-[214px]"
                 >
-                  <span class="block rounded-full bg-primary items-center pt-[6.5px] pb-[9px] pr-[7px] pl-2">
+                  <span class="block rounded-full bg-primary items-center pt-[6px] pb-[9px] pr-[7px] pl-2">
                     <Icon size={23} id="WhatsApp" />
                   </span>
-                  <span class="text-sm font-medium leading-[22.4px] text-right w-full pr-[19px]">
+                  <span class="text-sm font-medium leading-[22px] text-right w-full pr-[19px]">
                     {whatsApp.whatsAppButtonText}
                   </span>
                 </a>
                 <div class="flex flex-row items-center">
-                  <span class="font-medium leading-[22.4px] text-sm uppercase text-primary mr-[9px] md:flex hidden">
+                  <span class="font-medium leading-[22px] text-sm uppercase text-primary mr-[9px] md:flex hidden">
                     {followBolovoText}
                   </span>
                   {_social}
                 </div>
                 <div class="md:flex hidden flex-row items-center">
-                  <span class="font-medium leading-[22.4px] text-sm uppercase text-primary mr-1">
+                  <span class="font-medium leading-[22px] text-sm uppercase text-primary mr-1">
                     {changeCountryText}
                   </span>
                   <LanguageSwitcher
@@ -358,115 +341,14 @@ function Footer({
                   />
                 </div>
               </div>
-              <Divider class="border-primary" />
-              <div class="flex flex-col-reverse md:flex-row md:justify-center gap-[15px]">
+              <div class="flex flex-col-reverse md:flex-row md:justify-center gap-[15px] border-t border-primary">
                 <div
-                  class="text-center font-normal text-xs"
+                  class="text-center font-normal text-xs pt-[15px]"
                   dangerouslySetInnerHTML={{ __html: extraInfo }}
                 />
               </div>
             </div>
           </div>
-        )}
-        {layout?.variation == "Variation 2" && (
-          <div class="flex flex-col gap-10">
-            <div class="flex flex-col md:flex-row gap-10">
-              <div class="flex flex-col gap-10 lg:w-1/2">
-                {_logo}
-                {_social}
-                {_payments}
-                {_apps}
-                {_region}
-              </div>
-              <div class="flex flex-col gap-10 lg:gap-20 lg:w-1/2 lg:pr-10">
-                {_newsletter}
-                {_sectionLinks}
-              </div>
-            </div>
-            <Divider />
-            <div class="flex flex-col-reverse md:flex-row md:justify-between gap-10">
-              <PoweredByDeco />
-              {_links}
-            </div>
-          </div>
-        )}
-        {layout?.variation == "Variation 3" && (
-          <div class="flex flex-col gap-10">
-            {_logo}
-            <div class="flex flex-col lg:flex-row gap-14">
-              <div class="flex flex-col md:flex-row lg:flex-col md:justify-between lg:justify-normal gap-10 lg:w-2/5">
-                {_newsletter}
-                <div class="flex flex-col gap-10">
-                  {_payments}
-                  {_apps}
-                </div>
-              </div>
-              <div class="flex flex-col gap-10 lg:gap-20 lg:w-3/5 lg:items-end">
-                <div class="flex flex-col md:flex-row gap-10">
-                  {_sectionLinks}
-                  {_social}
-                </div>
-                {_region}
-              </div>
-            </div>
-            <Divider />
-            <div class="flex flex-col-reverse md:flex-row md:justify-between gap-10">
-              <PoweredByDeco />
-              {_links}
-            </div>
-          </div>
-        )}
-        {layout?.variation == "Variation 4" && (
-          <div class="flex flex-col gap-10">
-            {_newsletter}
-            {layout?.hide?.newsletter ? <></> : <Divider />}
-            <div class="flex flex-col lg:flex-row gap-10 lg:gap-20 lg:justify-between">
-              {_sectionLinks}
-              <div class="flex flex-col md:flex-row lg:flex-col gap-10 lg:gap-10 lg:w-2/5 lg:pl-10">
-                <div class="flex flex-col md:flex-row gap-10 lg:gap-20">
-                  <div class="lg:flex-auto">
-                    {_payments}
-                  </div>
-                  <div class="lg:flex-auto">
-                    {_social}
-                  </div>
-                </div>
-                <div class="flex flex-col gap-10 lg:gap-10">
-                  {_region}
-                  {_apps}
-                </div>
-              </div>
-            </div>
-            <Divider />
-            <div class="flex flex-col md:flex-row md:justify-between gap-10 md:items-center">
-              {_logo}
-              <PoweredByDeco />
-            </div>
-          </div>
-        )}
-        {layout?.variation == "Variation 5" && (
-          <div class="flex flex-col gap-10">
-            {_newsletter}
-            {layout?.hide?.newsletter ? <></> : <Divider />}
-            {_logo}
-            <div class="flex flex-col md:flex-row gap-10 lg:gap-20 md:justify-between">
-              {_sectionLinks}
-              <div class="flex flex-col gap-10 md:w-2/5 lg:pl-10">
-                {_payments}
-                {_social}
-                {_apps}
-              </div>
-            </div>
-            <Divider />
-            <div class="flex flex-col-reverse md:flex-row md:justify-between gap-10 md:items-center">
-              <PoweredByDeco />
-              <div class="flex flex-col md:flex-row gap-10 md:items-center">
-                {_links}
-                {_region}
-              </div>
-            </div>
-          </div>
-        )}
       </div>
       {layout?.hide?.backToTheTop
         ? <></>
