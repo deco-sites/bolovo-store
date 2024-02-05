d950f44aed01d3cdef502e9878630552055cc18f
ApMatheus <matheusaparecido101@gmail.com>
2024-02-05 15:17:29 -0300

    feat: function add cookies

--- components/header/Buttons/Language.tsx
+++ components/header/Buttons/Language.tsx
@@ -1,33 +1,94 @@
 import Icon from "deco-sites/bolovo-store/components/ui/Icon.tsx";
 import Image from "apps/website/components/Image.tsx";
-import { usePartialSection } from "deco/hooks/usePartialSection.ts";
+import { useSignal } from "@preact/signals";
 import type { Country } from "$store/components/header/Header.tsx";
+import { useEffect } from "preact/hooks";
+
+export interface Props {
+  countryFlag: Country[];
+  width?: number;
+  height?: number;
+  textClass?: string;
+  class?: string;
+}
 
 export default function LanguageSwitcher(
-  { countryFlag, width, height, textClass, class: _class = "" }: {
-    countryFlag: Country[];
-    width?: number;
-    height?: number;
-    textClass?: string;
-    class?: string;
-  },
+  { countryFlag, width, height, textClass, class: _class = "" }: Props,
 ) {
+  const cookieValue = useSignal(countryFlag[0].languageAbbreviation);
+
+  function setLanguageCookie(languageCode: string) {
+    document.cookie = "language=" + languageCode.toLowerCase() + "; path=/";
+    location.reload();
+  }
+  useEffect(() => {
+    async function teste() {
+      const cookieName = "language";
+
+      // Divide a string de cookies em pares chave/valor
+      const cookiesArray = document.cookie.split(";");
+
+      // Procura o cookie desejado na lista de cookies
+      for (let i = 0; i < cookiesArray.length; i++) {
+        const cookie = cookiesArray[i].trim();
+
+        // Verifica se o cookie começa com o nome desejado
+        if (cookie.startsWith(cookieName + "=")) {
+          // Retorna o valor do cookie
+          cookieValue.value = decodeURIComponent(
+            cookie.substring(cookieName.length + 1),
+          );
+          console.log("Cookies", cookieValue.value);
+          return; // Interrompe a execução após encontrar o cookie
+        }
+      }
+
+      // Se o cookie não for encontrado, use um valor padrão
+      cookieValue.value = countryFlag[0].languageAbbreviation;
+    }
+
+    teste();
+  }, [cookieValue.value]);
+
+  countryFlag.map((item) => {
+    console.log(
+      item.languageAbbreviation.toLowerCase() ===
+        cookieValue.value.toLowerCase(),
+      item.languageAbbreviation.toLowerCase(),
+      cookieValue.value.toLowerCase(),
+    );
+  });
+
   return (
     <>
-      <details class={`dropdown px-0 ${_class}`}>
+      <details
+        class={`dropdown px-0 ${_class}`}
+      >
         <summary class=" btn px-0 md:px-2 flex flex-row flex-grow md:flex-nowrap border-none hover:border-none justify-between md:justify-normal bg-transparent hover:bg-transparent w-full">
-          <div class="flex flex-row items-center">
-            <Image
-              src={countryFlag[0].countryImg.img}
-              alt={countryFlag[0].countryImg.alt}
-              width={width ?? 19}
-              height={height ?? 12}
-              class={`w-[${width}] h-[${height}] mr-1`}
-            />
-            <span class={`${textClass ?? "text-[15px]"} uppercase font-normal`}>
-              {countryFlag[0].countryLabel}
-            </span>
-          </div>
+          {countryFlag.map((iten) => (
+            iten.languageAbbreviation.toLowerCase() ===
+              cookieValue.value.toLowerCase() && (
+              <div
+                className="flex flex-row items-center"
+                key={iten.countryLabel}
+              >
+                <Image
+                  src={iten.countryImg.img}
+                  alt={iten.countryImg.alt}
+                  width={width ?? 19}
+                  height={height ?? 12}
+                  className={`w-[${width}] h-[${height}] mr-1`}
+                />
+                <span
+                  className={`${
+                    textClass ?? "text-[15px]"
+                  } uppercase font-normal`}
+                >
+                  {iten.countryLabel}
+                </span>
+              </div>
+            )
+          ))}
           <Icon
             id="ChevronDown"
             size={11}
@@ -38,9 +99,11 @@ export default function LanguageSwitcher(
         <ul class="p-2 shadow menu w-auto dropdown-content z-[1] bg-base-100 rounded-box">
           {countryFlag.map((iten) => {
             return (
-              <button class="cursor-pointer m-1 flex justify-center items-center uppercase gap-2 h-8 px-0 flex-row flex-nowrap border-none hover:border-none bg-transparent hover:bg-transparent w-max text-base font-normal" // {...usePartialSection({
+              <button
+                class="cursor-pointer m-1 flex justify-center items-center uppercase gap-2 h-8 px-0 flex-row flex-nowrap border-none hover:border-none bg-transparent hover:bg-transparent w-max text-base font-normal" // {...usePartialSection({
                 //   props: { Abbreviation: iten.languageAbbreviation },
                 // })}
+                onClick={() => setLanguageCookie(iten.languageAbbreviation)}
               >
                 <Image
                   src={iten.countryImg.img}
