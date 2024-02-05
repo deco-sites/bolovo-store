eb6495d86b3d681c50124d0dceeb16e9acd75504
soutofernando <fernandosoutol45@gmail.com>
2024-02-05 13:37:04 -0300

    Add components

--- sections/Institutional/InstitutionalPage.tsx
+++ sections/Institutional/InstitutionalPage.tsx
@@ -6,8 +6,9 @@ import type {Props as TextContentProps } from "deco-sites/bolovo-store/sections/
 
 export interface Props {
   title: string;
-  asideMenu: Section;
-  content: Section;
+  asideMenu: AsideProps;
+  /** @format html */
+  textContent: string;
 }
 
 function InstitutionalPage({
@@ -17,16 +18,13 @@ function InstitutionalPage({
 }: Props) {
   return (
     <>
-      <div class="flex flex-col lg:flex-row px-4 w-full mt-[15px]">
-        <div class="lg:max-w-[20%] max-w-full w-full">
-          <AsideComponent {...asideProps} />
-        </div>
-        <article class="lg:pl-[30px] pl-0 max-w-full lg:max-w-[80%] w-full">
-          <h3 class="hidden uppercase text-primary text-2xl font-medium leading-[36px] mb-5 border-b border-neutral-100 pb-[10px] lg:block">
+      <div class="flex flex-col lg:flex-row px-4 lg:container justify-between mt-[15px]">
+        <AsideMenu sectionMenu={asideMenu.sectionMenu} desktopButtonAlign={asideMenu.desktopButtonAlign} />
+        <article class="lg:pl-[30px] w-full">
+          <h3 class="hidden uppercase text-secondary text-2xl font-bold leading-[36.4px] mb-5 border-b border-neutral-100 pb-[10px] lg:block">
             {title}
           </h3>
-          {/* @ts-ignore opting for a ignore here so we can use a union type for the content section prop, and display it nicely in the admin panel */}
-          <ContentComponent {...contentProps} />
+          <TextContent content={textContent}/>
         </article>
       </div>
     </>
