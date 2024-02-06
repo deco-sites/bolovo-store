f5358967f156a868b0080fc1e31f510e474f0578
soutofernando <fernandosoutol45@gmail.com>
2024-02-06 14:13:21 -0300

    Fix component

--- sections/Institutional/AsideMenu.tsx
+++ sections/Institutional/AsideMenu.tsx
@@ -39,10 +39,6 @@ function AsideMenu(
     item.href === currentUrl
   );
 
-  if(!sectionMenu) {
-    return null 
-  }
-
   return (
     <aside class="w-full text-primary font-medium flex lg:justify-end">
       <ul class="lg:join join-vertical gap-[10px] w-full hidden">
@@ -83,10 +79,10 @@ function AsideMenu(
         ))}
       </ul>
       <div class="lg:hidden block w-full pb-5">
-        <div class="dropdown bg-prima w-full text-sm font-normal">
+        <div class="dropdown w-full text-sm font-normal">
           <label
             tabIndex={0}
-            class="btn btn-secondary btn-block justify-between border-none"
+            class="btn btn-primary btn-block justify-between border-none"
           >
             {currentRoute?.label ?? "Menu"}
             <Icon id="ChevronDown" width={26} height={26} />
@@ -104,10 +100,10 @@ function AsideMenu(
                         class={`
                       ${
                           currentUrl === item.href
-                            ? "bg-primary text-base-100 border-none"
-                            : "text-secondary"
+                            ? "bg-primary text-white border-none"
+                            : "text-primary"
                         }
-                      hover:bg-primary hover:text-base-100
+                      hover:bg-primary hover:text-white
                     `}
                         href={item.href}
                       >
