cd9b77e121af4a4b884767033ae92b09ecc2707a
soutofernando <fernandosoutol45@gmail.com>
2024-02-06 17:36:19 -0300

    Some adjustments

--- sections/Institutional/InstitutionalPage.tsx
+++ sections/Institutional/InstitutionalPage.tsx
@@ -17,13 +17,18 @@ function InstitutionalPage({
 }: Props) {
   return (
     <>
-      <div class="flex flex-col lg:flex-row px-4 lg:container justify-between mt-[15px]">
-        <AsideMenu sectionMenu={asideMenu.sectionMenu} desktopButtonAlign={asideMenu.desktopButtonAlign} />
-        <article class="lg:pl-[30px] w-full">
-          <h3 class="hidden uppercase text-secondary text-2xl font-bold leading-[36.4px] mb-5 border-b border-neutral-100 pb-[10px] lg:block">
+      <div class="flex flex-col lg:flex-row px-4 w-full mt-[15px]">
+        <div class="lg:max-w-[20%]">
+          <AsideComponent {...asideProps} />
+        </div>
+        <article class="lg:pl-[30px] lg:max-w-[80%] w-full">
+          <h3 class="hidden uppercase text-primary text-2xl font-medium leading-[36px] mb-5 border-b border-neutral-100 pb-[10px] lg:block">
             {title}
           </h3>
-          <TextContent content={textContent}/>
+          {/* @ts-ignore opting for a ignore here so we can use a union type for the content section prop, and display it nicely in the admin panel */}
+          <div class="mt-5 lg:mt-0">
+            <ContentComponent {...contentProps} />
+          </div>
         </article>
       </div>
     </>
