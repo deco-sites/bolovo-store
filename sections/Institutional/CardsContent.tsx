43edce99dd05a29478c53113b644bff4a4abac0c
soutofernando <fernandosoutol45@gmail.com>
2024-02-06 13:35:09 -0300

    Changing colors and styles

--- sections/Institutional/CardsContent.tsx
+++ sections/Institutional/CardsContent.tsx
@@ -1,15 +1,11 @@
-// import Icon, {
-//     AvailableIcons,
-//     PaymentIcons,
-//     SocialIcons,
-//   } from "$store/components/ui/InstitutionalIcon.tsx";
 import Icon from "deco-sites/bolovo-store/components/ui/Icon.tsx";
+import type { AvailableIcons } from "deco-sites/bolovo-store/components/ui/Icon.tsx"
   
   export interface Props {
     cards: {
       heading: {
         title: string;
-        // icon?: AvailableIcons | SocialIcons | PaymentIcons;
+        icon?: AvailableIcons;
       };
       content: {
         subtitle?: string;
@@ -18,7 +14,7 @@ import Icon from "deco-sites/bolovo-store/components/ui/Icon.tsx";
       links: {
         label: string;
         url: string;
-        // icon?: AvailableIcons | SocialIcons | PaymentIcons;
+        icon?: AvailableIcons;
       }[];
     }[];
   }
@@ -30,7 +26,7 @@ import Icon from "deco-sites/bolovo-store/components/ui/Icon.tsx";
           <div class="card card-bordered border-2 border-neutral-100 w-[360px]">
             <div class="card-body p-4">
               <div class="flex items-center gap-[10px] min-h-12 text-primary">
-                {/* <Icon id={card.heading.icon ?? "MapPin"} width={24} height={24} /> */}
+                <Icon id={card.heading.icon ?? "Instagram"}  width={24} height={24} />
                 <h6 class="font-medium max-w-[87%] break-words">
                   {card.heading.title}
                 </h6>
@@ -45,12 +41,12 @@ import Icon from "deco-sites/bolovo-store/components/ui/Icon.tsx";
               <div class="flex flex-col gap-[6px] items-start font-bold text-base-content">
                 {card.links.map((link) => (
                   <a href={link.url} class="flex items-center gap-[10px] text-sm">
-                    {/* <Icon
-                      id={link.icon ?? "Phone"}
+                    <Icon
+                      id={link.icon ?? "Instagram"}
                       width={18}
                       height={18}
                       strokeWidth={2}
-                    /> */}
+                    />
                     <span>{link.label}</span>
                   </a>
                 ))}
