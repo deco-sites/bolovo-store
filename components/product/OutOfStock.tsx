9bc663c327d6ec292d5e3e22f0df652695846b35
ApMatheus <matheusaparecido101@gmail.com>
2024-02-13 19:33:07 -0300

    feat: validation language

--- components/product/OutOfStock.tsx
+++ components/product/OutOfStock.tsx
@@ -32,25 +32,25 @@ function Notify({ productID }: Props) {
   return (
     <form class="form-control justify-start gap-2" onSubmit={handleSubmit}>
       <span class="text-base">
-        {activePriceIntl
+        {activePriceIntl.value
           ? "This product is currently unavailable."
           : "Este produto está indisponivel no momento"}
       </span>
       <span class="text-sm">
-        {activePriceIntl
+        {activePriceIntl.value
           ? "Notify me when it becomes available."
           : "Avise-me quando estiver disponivel"}
       </span>
 
       <input
-        placeholder={activePriceIntl ? "Name" : "Nome"}
+        placeholder={activePriceIntl.value ? "Name" : "Nome"}
         class="input input-bordered"
         name="name"
       />
       <input placeholder="Email" class="input input-bordered" name="email" />
 
       <button class="btn disabled:loading" disabled={loading}>
-        {activePriceIntl ? "Send" : "Enviar"}
+        {activePriceIntl.value ? "Send" : "Enviar"}
       </button>
     </form>
   );
