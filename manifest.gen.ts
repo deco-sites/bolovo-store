502695a62b4afa68f937c2656f6c844de0581304
soutofernando <fernandosoutol45@gmail.com>
2024-02-16 23:59:06 -0300

    Remove sections and components unused

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -7,45 +7,37 @@ import * as $$$1 from "./loaders/List/Sections.tsx";
 import * as $$$2 from "./loaders/newsletter.ts";
 import * as $$$$$$0 from "./sections/Category/BannerCategoryGrid.tsx";
 import * as $$$$$$1 from "./sections/Category/CategoryBanner.tsx";
-import * as $$$$$$2 from "./sections/Category/CategoryList.tsx";
-import * as $$$$$$3 from "./sections/Category/CategoryResult.tsx";
-import * as $$$$$$4 from "./sections/Content/BannerGrid.tsx";
-import * as $$$$$$5 from "./sections/Content/Stores.tsx";
-import * as $$$$$$6 from "./sections/Content/TextSEO.tsx";
-import * as $$$$$$7 from "./sections/Content/VideoAndText.tsx";
-import * as $$$$$$8 from "./sections/Footer/Footer.tsx";
-import * as $$$$$$9 from "./sections/Gallery.tsx";
-import * as $$$$$$10 from "./sections/Header/Header.tsx";
-import * as $$$$$$11 from "./sections/Images/BannerGrid.tsx";
-import * as $$$$$$12 from "./sections/Images/Carousel.tsx";
-import * as $$$$$$13 from "./sections/Images/ImageGallery.tsx";
-import * as $$$$$$14 from "./sections/Images/ShoppableBanner.tsx";
-import * as $$$$$$15 from "./sections/Layout/Container.tsx";
-import * as $$$$$$16 from "./sections/Layout/Flex.tsx";
-import * as $$$$$$17 from "./sections/Layout/Grid.tsx";
-import * as $$$$$$18 from "./sections/Layout/GridItem.tsx";
-import * as $$$$$$19 from "./sections/Links/LinkTree.tsx";
-import * as $$$$$$20 from "./sections/Links/Shortcuts.tsx";
-import * as $$$$$$21 from "./sections/Miscellaneous/CampaignTimer.tsx";
-import * as $$$$$$22 from "./sections/Miscellaneous/CookieConsent.tsx";
-import * as $$$$$$23 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$24 from "./sections/Product/FeaturedGallery.tsx";
-import * as $$$$$$25 from "./sections/Product/ImageGalleryFrontBack.tsx";
-import * as $$$$$$26 from "./sections/Product/ImageGallerySlider.tsx";
-import * as $$$$$$27 from "./sections/Product/NotFound.tsx";
-import * as $$$$$$28 from "./sections/Product/NotFoundChallenge.tsx";
-import * as $$$$$$29 from "./sections/Product/PageOfProduct.tsx";
-import * as $$$$$$30 from "./sections/Product/PhotoGallery.tsx";
-import * as $$$$$$31 from "./sections/Product/ProductGallery.tsx";
-import * as $$$$$$32 from "./sections/Product/ProductInfo.tsx";
-import * as $$$$$$33 from "./sections/Product/ProductRelatedShelf.tsx";
-import * as $$$$$$34 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$35 from "./sections/Product/ProductShelfTabbed.tsx";
-import * as $$$$$$36 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$37 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$38 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$39 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$40 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$2 from "./sections/Category/CategoryResult.tsx";
+import * as $$$$$$3 from "./sections/Content/BannerGrid.tsx";
+import * as $$$$$$4 from "./sections/Content/Stores.tsx";
+import * as $$$$$$5 from "./sections/Content/TextSEO.tsx";
+import * as $$$$$$6 from "./sections/Content/VideoAndText.tsx";
+import * as $$$$$$7 from "./sections/Footer/Footer.tsx";
+import * as $$$$$$8 from "./sections/Gallery.tsx";
+import * as $$$$$$9 from "./sections/Header/Header.tsx";
+import * as $$$$$$10 from "./sections/Images/Carousel.tsx";
+import * as $$$$$$11 from "./sections/Layout/Container.tsx";
+import * as $$$$$$12 from "./sections/Layout/Flex.tsx";
+import * as $$$$$$13 from "./sections/Layout/Grid.tsx";
+import * as $$$$$$14 from "./sections/Layout/GridItem.tsx";
+import * as $$$$$$15 from "./sections/Newsletter/Newsletter.tsx";
+import * as $$$$$$16 from "./sections/Product/FeaturedGallery.tsx";
+import * as $$$$$$17 from "./sections/Product/ImageGalleryFrontBack.tsx";
+import * as $$$$$$18 from "./sections/Product/ImageGallerySlider.tsx";
+import * as $$$$$$19 from "./sections/Product/NotFound.tsx";
+import * as $$$$$$20 from "./sections/Product/NotFoundChallenge.tsx";
+import * as $$$$$$21 from "./sections/Product/PageOfProduct.tsx";
+import * as $$$$$$22 from "./sections/Product/PhotoGallery.tsx";
+import * as $$$$$$23 from "./sections/Product/ProductGallery.tsx";
+import * as $$$$$$24 from "./sections/Product/ProductInfo.tsx";
+import * as $$$$$$25 from "./sections/Product/ProductRelatedShelf.tsx";
+import * as $$$$$$26 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$27 from "./sections/Product/ProductShelfTabbed.tsx";
+import * as $$$$$$28 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$29 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$30 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$31 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$32 from "./sections/Theme/Theme.tsx";
 import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
 
@@ -58,49 +50,39 @@ const manifest = {
   "sections": {
     "deco-sites/bolovo-store/sections/Category/BannerCategoryGrid.tsx": $$$$$$0,
     "deco-sites/bolovo-store/sections/Category/CategoryBanner.tsx": $$$$$$1,
-    "deco-sites/bolovo-store/sections/Category/CategoryList.tsx": $$$$$$2,
-    "deco-sites/bolovo-store/sections/Category/CategoryResult.tsx": $$$$$$3,
-    "deco-sites/bolovo-store/sections/Content/BannerGrid.tsx": $$$$$$4,
-    "deco-sites/bolovo-store/sections/Content/Stores.tsx": $$$$$$5,
-    "deco-sites/bolovo-store/sections/Content/TextSEO.tsx": $$$$$$6,
-    "deco-sites/bolovo-store/sections/Content/VideoAndText.tsx": $$$$$$7,
-    "deco-sites/bolovo-store/sections/Footer/Footer.tsx": $$$$$$8,
-    "deco-sites/bolovo-store/sections/Gallery.tsx": $$$$$$9,
-    "deco-sites/bolovo-store/sections/Header/Header.tsx": $$$$$$10,
-    "deco-sites/bolovo-store/sections/Images/BannerGrid.tsx": $$$$$$11,
-    "deco-sites/bolovo-store/sections/Images/Carousel.tsx": $$$$$$12,
-    "deco-sites/bolovo-store/sections/Images/ImageGallery.tsx": $$$$$$13,
-    "deco-sites/bolovo-store/sections/Images/ShoppableBanner.tsx": $$$$$$14,
-    "deco-sites/bolovo-store/sections/Layout/Container.tsx": $$$$$$15,
-    "deco-sites/bolovo-store/sections/Layout/Flex.tsx": $$$$$$16,
-    "deco-sites/bolovo-store/sections/Layout/Grid.tsx": $$$$$$17,
-    "deco-sites/bolovo-store/sections/Layout/GridItem.tsx": $$$$$$18,
-    "deco-sites/bolovo-store/sections/Links/LinkTree.tsx": $$$$$$19,
-    "deco-sites/bolovo-store/sections/Links/Shortcuts.tsx": $$$$$$20,
-    "deco-sites/bolovo-store/sections/Miscellaneous/CampaignTimer.tsx":
-      $$$$$$21,
-    "deco-sites/bolovo-store/sections/Miscellaneous/CookieConsent.tsx":
-      $$$$$$22,
-    "deco-sites/bolovo-store/sections/Newsletter/Newsletter.tsx": $$$$$$23,
-    "deco-sites/bolovo-store/sections/Product/FeaturedGallery.tsx": $$$$$$24,
+    "deco-sites/bolovo-store/sections/Category/CategoryResult.tsx": $$$$$$2,
+    "deco-sites/bolovo-store/sections/Content/BannerGrid.tsx": $$$$$$3,
+    "deco-sites/bolovo-store/sections/Content/Stores.tsx": $$$$$$4,
+    "deco-sites/bolovo-store/sections/Content/TextSEO.tsx": $$$$$$5,
+    "deco-sites/bolovo-store/sections/Content/VideoAndText.tsx": $$$$$$6,
+    "deco-sites/bolovo-store/sections/Footer/Footer.tsx": $$$$$$7,
+    "deco-sites/bolovo-store/sections/Gallery.tsx": $$$$$$8,
+    "deco-sites/bolovo-store/sections/Header/Header.tsx": $$$$$$9,
+    "deco-sites/bolovo-store/sections/Images/Carousel.tsx": $$$$$$10,
+    "deco-sites/bolovo-store/sections/Layout/Container.tsx": $$$$$$11,
+    "deco-sites/bolovo-store/sections/Layout/Flex.tsx": $$$$$$12,
+    "deco-sites/bolovo-store/sections/Layout/Grid.tsx": $$$$$$13,
+    "deco-sites/bolovo-store/sections/Layout/GridItem.tsx": $$$$$$14,
+    "deco-sites/bolovo-store/sections/Newsletter/Newsletter.tsx": $$$$$$15,
+    "deco-sites/bolovo-store/sections/Product/FeaturedGallery.tsx": $$$$$$16,
     "deco-sites/bolovo-store/sections/Product/ImageGalleryFrontBack.tsx":
-      $$$$$$25,
-    "deco-sites/bolovo-store/sections/Product/ImageGallerySlider.tsx": $$$$$$26,
-    "deco-sites/bolovo-store/sections/Product/NotFound.tsx": $$$$$$27,
-    "deco-sites/bolovo-store/sections/Product/NotFoundChallenge.tsx": $$$$$$28,
-    "deco-sites/bolovo-store/sections/Product/PageOfProduct.tsx": $$$$$$29,
-    "deco-sites/bolovo-store/sections/Product/PhotoGallery.tsx": $$$$$$30,
-    "deco-sites/bolovo-store/sections/Product/ProductGallery.tsx": $$$$$$31,
-    "deco-sites/bolovo-store/sections/Product/ProductInfo.tsx": $$$$$$32,
+      $$$$$$17,
+    "deco-sites/bolovo-store/sections/Product/ImageGallerySlider.tsx": $$$$$$18,
+    "deco-sites/bolovo-store/sections/Product/NotFound.tsx": $$$$$$19,
+    "deco-sites/bolovo-store/sections/Product/NotFoundChallenge.tsx": $$$$$$20,
+    "deco-sites/bolovo-store/sections/Product/PageOfProduct.tsx": $$$$$$21,
+    "deco-sites/bolovo-store/sections/Product/PhotoGallery.tsx": $$$$$$22,
+    "deco-sites/bolovo-store/sections/Product/ProductGallery.tsx": $$$$$$23,
+    "deco-sites/bolovo-store/sections/Product/ProductInfo.tsx": $$$$$$24,
     "deco-sites/bolovo-store/sections/Product/ProductRelatedShelf.tsx":
-      $$$$$$33,
-    "deco-sites/bolovo-store/sections/Product/ProductShelf.tsx": $$$$$$34,
-    "deco-sites/bolovo-store/sections/Product/ProductShelfTabbed.tsx": $$$$$$35,
-    "deco-sites/bolovo-store/sections/Product/SearchResult.tsx": $$$$$$36,
-    "deco-sites/bolovo-store/sections/Product/Wishlist.tsx": $$$$$$37,
-    "deco-sites/bolovo-store/sections/Social/InstagramPosts.tsx": $$$$$$38,
-    "deco-sites/bolovo-store/sections/Social/WhatsApp.tsx": $$$$$$39,
-    "deco-sites/bolovo-store/sections/Theme/Theme.tsx": $$$$$$40,
+      $$$$$$25,
+    "deco-sites/bolovo-store/sections/Product/ProductShelf.tsx": $$$$$$26,
+    "deco-sites/bolovo-store/sections/Product/ProductShelfTabbed.tsx": $$$$$$27,
+    "deco-sites/bolovo-store/sections/Product/SearchResult.tsx": $$$$$$28,
+    "deco-sites/bolovo-store/sections/Product/Wishlist.tsx": $$$$$$29,
+    "deco-sites/bolovo-store/sections/Social/InstagramPosts.tsx": $$$$$$30,
+    "deco-sites/bolovo-store/sections/Social/WhatsApp.tsx": $$$$$$31,
+    "deco-sites/bolovo-store/sections/Theme/Theme.tsx": $$$$$$32,
   },
   "apps": {
     "deco-sites/bolovo-store/apps/decohub.ts": $$$$$$$$$$$0,
