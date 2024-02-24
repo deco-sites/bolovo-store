45e703f98b5c5dcd7ebd6cd39d17d85213c74d04
Martin Adam <martin.viana99@gmail.com>
2024-02-24 12:47:21 -0300

    Feat: QA task ProductRelatedShelf

--- components/product/ProductRelatedShelf.tsx
+++ components/product/ProductRelatedShelf.tsx
@@ -1,6 +1,7 @@
 import { AppContext } from "apps/vnda/mod.ts";
 import type { Product, ProductDetailsPage } from "apps/commerce/types.ts";
 import ProductShelf from "$store/components/product/ProductShelf.tsx";
+import { Color } from "$store/components/search/SearchResult.tsx";
 
 export interface Props {
   title?: string;
@@ -13,6 +14,9 @@ export interface Props {
     link: string;
   };
   relatedProducts?: ProductDetailsPage | null;
+  colors: Color[];
+  /** @description Choose if you would like to showcase the color variants in the product cards  */
+  showColorVariants?: boolean;
 }
 
 export const loader = async (props: Props, req: Request, ctx: AppContext) => {
@@ -23,43 +27,90 @@ export const loader = async (props: Props, req: Request, ctx: AppContext) => {
 
   const tag = `relacionados-${category?.toLowerCase()}`;
 
+  console.log("TAG:", tag);
+
+  let data: Product[] = [];
+
   if (!tag) {
-    const allTagData = await ctx.get({
+    const allTagData = await ctx.get<Product[]>({  
       "__resolveType": "vnda/loaders/productList.ts",
       "tags": "todos",
     });
 
-    return {
-      ...props,
-      relatedProductsList: allTagData ?? [],
-    };
+    data = allTagData ?? [];
+  } else {
+    data = await ctx.get<Product[]>({  
+      "__resolveType": "vnda/loaders/productList.ts",
+      "tags": tag,
+    });
   }
 
-  const data = await ctx.get({
-    "__resolveType": "vnda/loaders/productList.ts",
-    "tags": tag,
-  });
+  const { showColorVariants } = props;
+  const colorRelated: { [productName: string]: Product[] } = {};
+
+  if (showColorVariants) {
+    for (const product of data || []) {
+      let camisetaVariantProperty;
+
+      for (const property of product.additionalProperty || []) {
+        if (property.valueReference === "TAGS") {
+          try {
+            const data = JSON.parse(property.value || "");
+
+            if (data.type === "variante_cor") {
+              camisetaVariantProperty = data.name;
+              break;
+            }
+          } catch (error) {
+            console.error("Erro ao fazer parse do valor como JSON:", error);
+          }
+        }
+      }
+
+      if (camisetaVariantProperty) {
+        const productList = await ctx.get<Product[]>({ 
+          "__resolveType": "vnda/loaders/productList.ts",
+          "typeTags": [{ key: "variante_cor", value: camisetaVariantProperty }],
+        });
+
+        if (product.name !== undefined && Array.isArray(productList)) {
+          colorRelated[product.name] = productList;
+        }
+      }
+    }
+  }
 
   return {
     ...props,
     relatedProductsList: data ?? [],
+    colorVariant: colorRelated || {}
   };
 };
+
+
 
 function ProductRelatedShelf({
   relatedProductsList,
   title,
   layout,
   seeMore,
+  colors, 
+  colorVariant, 
+  showColorVariants
 }:
   & Props
-  & { relatedProductsList: Product[] }) {
+  & { relatedProductsList: Product[] }
+  & { colorVariant: { [productName: string]: Product[] } }
+) {
   return (
     <ProductShelf
       products={relatedProductsList}
       title={title}
       layout={layout}
       seeMore={seeMore}
+      colorVariant={colorVariant}
+      colors={colors}
+      showColorVariants={showColorVariants}
     />
   );
 }
