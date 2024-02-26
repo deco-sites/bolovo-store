bc1840e9cb8c5429104128765f8d15a6f0e63358
Martin Adam <martin.viana99@gmail.com>
2024-02-26 12:23:48 -0300

    QA: Newsletter API

--- loaders/newsletter.ts
+++ loaders/newsletter.ts
@@ -10,22 +10,31 @@ const newsletter = async (
   _req: Request,
   ctx: AppContext,
 ): Promise<number> => {
-  const form = new FormData();
-  const baseUrl = ctx.commerce?.platform == "vnda"
-    ? `${ctx.commerce?.publicUrl}webform`
-    : "";
   const { email } = props;
-  form.append("key", "bolovo-newsletter");
-  form.append("reply_to", email);
-  form.append("vnda", "");
-  form.append("email", email);
-
-  const response = await fetchSafe(baseUrl, {
-    method: "POST",
-    body: form,
-  });
-
-  return response.status;
+  
+  if (ctx.commerce?.platform === "vnda") {
+    const baseUrl = `${ctx.commerce.publicUrl}webform`;
+    
+    const formData = new URLSearchParams();
+    formData.append("key", "bolovo-newsletter");
+    formData.append("reply_to", email);
+    formData.append("vnda", "");
+    formData.append("email", email);
+    
+    const requestOptions: RequestInit = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+      },
+      body: formData.toString(), 
+    };
+    
+    const response = await fetchSafe(baseUrl, requestOptions);
+    
+    return response.status;
+  } else {
+    return 400; // Bad Request
+  }
 };
 
 export default newsletter;
