d77a367938e2a98bf3945fc76789469b60134c5b
Martin Adam <martin.viana99@gmail.com>
2024-02-29 02:11:52 -0300

    Mobile update

--- components/content/StoresList.tsx
+++ components/content/StoresList.tsx
@@ -23,56 +23,56 @@ function StoresList(
 
   return (
     <div class="w-full py-8 flex flex-col gap-5 px-[15px] mx-auto lg:gap-16 lg:py-10">
-      <h2 className="text-[#121212] text-[26px] leading-[26px] text-center font-bold">
+      <h2 className="text-[#121212] text-[20px] leading-[26px] text-center font-bold">
         {title}
       </h2>
-      <div class="w-full flex">
-      <div class="w-1/2">
-        <img src="https://cdn.vnda.com.br/bolovo/2020/12/02/15_12_7_752_contatolucas1.jpg?v=1709056697"/>
-      </div>
-      <div
-        id={id}
-        class="w-full grid grid-rows-[40px_1fr_40px] lg:px-[15px] gap-y-6"
-      >
-        <>
-        <div class="flex justify-between w-full px-5">
-          <div class="hidden relative lg:block z-10 col-start-1 row-start-2">
-            <Slider.PrevButton class="btn btn-circle btn-ghost !bg-[transparent] right-1/2">
-              <Icon size={24} id="ArrowPointingLeft" strokeWidth={3} />
-            </Slider.PrevButton>
-          </div>
-          <div class="hidden relative lg:block z-10 col-start-3 row-start-2">
-            <Slider.NextButton class="btn btn-circle btn-ghost !bg-[transparent] left-1/2">
-              <Icon size={24} id="ArrowPointingRight" strokeWidth={3} />
-            </Slider.NextButton>
-          </div>
-          </div>
-        </>
-        <SliderJS rootId={id} />
-        <Slider class="mx-4 w-full carousel carousel-start gap-6 lg:gap-[15px] col-span-full row-start-2 row-end-5">
-          {stores?.map((store, index) => (
-            <Slider.Item
-              index={index}
-              class="flex flex-col carousel-item w-[38.605vw] lg:w-[calc((100%-46px)/4)] sm:first:pl-0 sm:last:pr-0"
-            >
-              <h4 className="text-[#121212] text-[14px] font-semibold mb-[25px] uppercase">
-                {store.storeState}
-              </h4>
-              <ul class="flex flex-col gap-8 pr-8">
-                {store.address?.map((address) => (
+      <div class="w-full flex flex-col-reverse lg:flex-row items-center">
+        <div class="w-1/2">
+          <img src="https://cdn.vnda.com.br/bolovo/2020/12/02/15_12_7_752_contatolucas1.jpg?v=1709056697" />
+        </div>
+        <div
+          id={id}
+          class="w-full grid grid-rows-[40px_1fr_40px] lg:px-[15px] gap-y-6"
+        >
+          <>
+            <div class="flex justify-between w-full px-5">
+              <div class="relative block z-10 col-start-1 row-start-2">
+                <Slider.PrevButton class="btn btn-circle btn-ghost !bg-[transparent] right-1/2">
+                  <Icon size={24} id="ArrowPointingLeft" strokeWidth={3} />
+                </Slider.PrevButton>
+              </div>
+              <div class="relative block z-10 col-start-3 row-start-2">
+                <Slider.NextButton class="btn btn-circle btn-ghost !bg-[transparent] left-1/2">
+                  <Icon size={24} id="ArrowPointingRight" strokeWidth={3} />
+                </Slider.NextButton>
+              </div>
+            </div>
+          </>
+          {/* <Slider.Progress /> */}
+          <SliderJS rootId={id} />
+          <Slider class="mx-4 w-full carousel carousel-start gap-6 lg:gap-[15px] col-span-full row-start-2 row-end-5">
+            {stores?.map((store, index) => (
+              <Slider.Item
+                index={index}
+                class="flex flex-col carousel-item w-[38.605vw] lg:w-[calc((100%-46px)/4)] sm:first:pl-0 sm:last:pr-0"
+              >
+                <h4 className="text-[#121212] text-[14px] font-semibold mb-[25px] uppercase">
+                  {store.storeState}
+                </h4>
+                <ul class="flex flex-col gap-8 pr-8">
+                  {store.address?.map((address) => (
                     <div
-                    className="font-acumin lg:text-base-300 text-[12px]"
-                    dangerouslySetInnerHTML={{ __html: address ?? "" }}
+                      className="font-acumin lg:text-base-300 text-[12px]"
+                      dangerouslySetInnerHTML={{ __html: address ?? "" }}
                     />
-                ))}
-              </ul>
-            </Slider.Item>
-          ))}
-        </Slider>
-      </div>
+                  ))}
+                </ul>
+              </Slider.Item>
+            ))}
+          </Slider>
+        </div>
       </div>
     </div>
-
   );
 }
 
--- manifest.gen.ts
+++ manifest.gen.ts
@@ -21,33 +21,34 @@ import * as $$$$$$11 from "./sections/Institutional/AccordionsContent.tsx";
 import * as $$$$$$12 from "./sections/Institutional/AsideMenu.tsx";
 import * as $$$$$$13 from "./sections/Institutional/CardsContent.tsx";
 import * as $$$$$$14 from "./sections/Institutional/ContactForm.tsx";
-import * as $$$$$$15 from "./sections/Institutional/InstitutionalPage.tsx";
-import * as $$$$$$16 from "./sections/Institutional/StoresList.tsx";
-import * as $$$$$$17 from "./sections/Institutional/TextContent.tsx";
-import * as $$$$$$18 from "./sections/Layout/Container.tsx";
-import * as $$$$$$19 from "./sections/Layout/Flex.tsx";
-import * as $$$$$$20 from "./sections/Layout/Grid.tsx";
-import * as $$$$$$21 from "./sections/Layout/GridItem.tsx";
-import * as $$$$$$22 from "./sections/Miscellaneous/CookieConsent.tsx";
-import * as $$$$$$23 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$24 from "./sections/Product/FeaturedGallery.tsx";
-import * as $$$$$$25 from "./sections/Product/ImageGalleryFrontBack.tsx";
-import * as $$$$$$26 from "./sections/Product/ImageGallerySlider.tsx";
-import * as $$$$$$27 from "./sections/Product/NotFound.tsx";
-import * as $$$$$$28 from "./sections/Product/NotFoundChallenge.tsx";
-import * as $$$$$$29 from "./sections/Product/PageOfProduct.tsx";
-import * as $$$$$$30 from "./sections/Product/PhotoGallery.tsx";
-import * as $$$$$$31 from "./sections/Product/ProductDetails.tsx";
-import * as $$$$$$32 from "./sections/Product/ProductGallery.tsx";
-import * as $$$$$$33 from "./sections/Product/ProductInfo.tsx";
-import * as $$$$$$34 from "./sections/Product/ProductRelatedShelf.tsx";
-import * as $$$$$$35 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$36 from "./sections/Product/ProductShelfTabbed.tsx";
-import * as $$$$$$37 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$38 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$39 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$40 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$41 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$15 from "./sections/Institutional/FeaturedStore.tsx";
+import * as $$$$$$16 from "./sections/Institutional/InstitutionalPage.tsx";
+import * as $$$$$$17 from "./sections/Institutional/StoresList.tsx";
+import * as $$$$$$18 from "./sections/Institutional/TextContent.tsx";
+import * as $$$$$$19 from "./sections/Layout/Container.tsx";
+import * as $$$$$$20 from "./sections/Layout/Flex.tsx";
+import * as $$$$$$21 from "./sections/Layout/Grid.tsx";
+import * as $$$$$$22 from "./sections/Layout/GridItem.tsx";
+import * as $$$$$$23 from "./sections/Miscellaneous/CookieConsent.tsx";
+import * as $$$$$$24 from "./sections/Newsletter/Newsletter.tsx";
+import * as $$$$$$25 from "./sections/Product/FeaturedGallery.tsx";
+import * as $$$$$$26 from "./sections/Product/ImageGalleryFrontBack.tsx";
+import * as $$$$$$27 from "./sections/Product/ImageGallerySlider.tsx";
+import * as $$$$$$28 from "./sections/Product/NotFound.tsx";
+import * as $$$$$$29 from "./sections/Product/NotFoundChallenge.tsx";
+import * as $$$$$$30 from "./sections/Product/PageOfProduct.tsx";
+import * as $$$$$$31 from "./sections/Product/PhotoGallery.tsx";
+import * as $$$$$$32 from "./sections/Product/ProductDetails.tsx";
+import * as $$$$$$33 from "./sections/Product/ProductGallery.tsx";
+import * as $$$$$$34 from "./sections/Product/ProductInfo.tsx";
+import * as $$$$$$35 from "./sections/Product/ProductRelatedShelf.tsx";
+import * as $$$$$$36 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$37 from "./sections/Product/ProductShelfTabbed.tsx";
+import * as $$$$$$38 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$39 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$40 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$41 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$42 from "./sections/Theme/Theme.tsx";
 import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
 
@@ -75,37 +76,39 @@ const manifest = {
     "deco-sites/bolovo-store/sections/Institutional/AsideMenu.tsx": $$$$$$12,
     "deco-sites/bolovo-store/sections/Institutional/CardsContent.tsx": $$$$$$13,
     "deco-sites/bolovo-store/sections/Institutional/ContactForm.tsx": $$$$$$14,
-    "deco-sites/bolovo-store/sections/Institutional/InstitutionalPage.tsx":
+    "deco-sites/bolovo-store/sections/Institutional/FeaturedStore.tsx":
       $$$$$$15,
-    "deco-sites/bolovo-store/sections/Institutional/StoresList.tsx": $$$$$$16,
-    "deco-sites/bolovo-store/sections/Institutional/TextContent.tsx": $$$$$$17,
-    "deco-sites/bolovo-store/sections/Layout/Container.tsx": $$$$$$18,
-    "deco-sites/bolovo-store/sections/Layout/Flex.tsx": $$$$$$19,
-    "deco-sites/bolovo-store/sections/Layout/Grid.tsx": $$$$$$20,
-    "deco-sites/bolovo-store/sections/Layout/GridItem.tsx": $$$$$$21,
+    "deco-sites/bolovo-store/sections/Institutional/InstitutionalPage.tsx":
+      $$$$$$16,
+    "deco-sites/bolovo-store/sections/Institutional/StoresList.tsx": $$$$$$17,
+    "deco-sites/bolovo-store/sections/Institutional/TextContent.tsx": $$$$$$18,
+    "deco-sites/bolovo-store/sections/Layout/Container.tsx": $$$$$$19,
+    "deco-sites/bolovo-store/sections/Layout/Flex.tsx": $$$$$$20,
+    "deco-sites/bolovo-store/sections/Layout/Grid.tsx": $$$$$$21,
+    "deco-sites/bolovo-store/sections/Layout/GridItem.tsx": $$$$$$22,
     "deco-sites/bolovo-store/sections/Miscellaneous/CookieConsent.tsx":
-      $$$$$$22,
-    "deco-sites/bolovo-store/sections/Newsletter/Newsletter.tsx": $$$$$$23,
-    "deco-sites/bolovo-store/sections/Product/FeaturedGallery.tsx": $$$$$$24,
+      $$$$$$23,
+    "deco-sites/bolovo-store/sections/Newsletter/Newsletter.tsx": $$$$$$24,
+    "deco-sites/bolovo-store/sections/Product/FeaturedGallery.tsx": $$$$$$25,
     "deco-sites/bolovo-store/sections/Product/ImageGalleryFrontBack.tsx":
-      $$$$$$25,
-    "deco-sites/bolovo-store/sections/Product/ImageGallerySlider.tsx": $$$$$$26,
-    "deco-sites/bolovo-store/sections/Product/NotFound.tsx": $$$$$$27,
-    "deco-sites/bolovo-store/sections/Product/NotFoundChallenge.tsx": $$$$$$28,
-    "deco-sites/bolovo-store/sections/Product/PageOfProduct.tsx": $$$$$$29,
-    "deco-sites/bolovo-store/sections/Product/PhotoGallery.tsx": $$$$$$30,
-    "deco-sites/bolovo-store/sections/Product/ProductDetails.tsx": $$$$$$31,
-    "deco-sites/bolovo-store/sections/Product/ProductGallery.tsx": $$$$$$32,
-    "deco-sites/bolovo-store/sections/Product/ProductInfo.tsx": $$$$$$33,
+      $$$$$$26,
+    "deco-sites/bolovo-store/sections/Product/ImageGallerySlider.tsx": $$$$$$27,
+    "deco-sites/bolovo-store/sections/Product/NotFound.tsx": $$$$$$28,
+    "deco-sites/bolovo-store/sections/Product/NotFoundChallenge.tsx": $$$$$$29,
+    "deco-sites/bolovo-store/sections/Product/PageOfProduct.tsx": $$$$$$30,
+    "deco-sites/bolovo-store/sections/Product/PhotoGallery.tsx": $$$$$$31,
+    "deco-sites/bolovo-store/sections/Product/ProductDetails.tsx": $$$$$$32,
+    "deco-sites/bolovo-store/sections/Product/ProductGallery.tsx": $$$$$$33,
+    "deco-sites/bolovo-store/sections/Product/ProductInfo.tsx": $$$$$$34,
     "deco-sites/bolovo-store/sections/Product/ProductRelatedShelf.tsx":
-      $$$$$$34,
-    "deco-sites/bolovo-store/sections/Product/ProductShelf.tsx": $$$$$$35,
-    "deco-sites/bolovo-store/sections/Product/ProductShelfTabbed.tsx": $$$$$$36,
-    "deco-sites/bolovo-store/sections/Product/SearchResult.tsx": $$$$$$37,
-    "deco-sites/bolovo-store/sections/Product/Wishlist.tsx": $$$$$$38,
-    "deco-sites/bolovo-store/sections/Social/InstagramPosts.tsx": $$$$$$39,
-    "deco-sites/bolovo-store/sections/Social/WhatsApp.tsx": $$$$$$40,
-    "deco-sites/bolovo-store/sections/Theme/Theme.tsx": $$$$$$41,
+      $$$$$$35,
+    "deco-sites/bolovo-store/sections/Product/ProductShelf.tsx": $$$$$$36,
+    "deco-sites/bolovo-store/sections/Product/ProductShelfTabbed.tsx": $$$$$$37,
+    "deco-sites/bolovo-store/sections/Product/SearchResult.tsx": $$$$$$38,
+    "deco-sites/bolovo-store/sections/Product/Wishlist.tsx": $$$$$$39,
+    "deco-sites/bolovo-store/sections/Social/InstagramPosts.tsx": $$$$$$40,
+    "deco-sites/bolovo-store/sections/Social/WhatsApp.tsx": $$$$$$41,
+    "deco-sites/bolovo-store/sections/Theme/Theme.tsx": $$$$$$42,
   },
   "apps": {
     "deco-sites/bolovo-store/apps/decohub.ts": $$$$$$$$$$$0,
