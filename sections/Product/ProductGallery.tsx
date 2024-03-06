ed00b8a4096b910e5f7f9e1065090c0d69578466
soutofernando <fernandosoutol45@gmail.com>
2024-03-05 21:14:25 -0300

    Add async loading

--- sections/Product/ProductGallery.tsx
+++ sections/Product/ProductGallery.tsx
@@ -9,6 +9,14 @@ export interface Props {
   products: Product[] | null;
 }
 
+export function LoadingFallback() {
+  return (
+    <div style={{ height: "716px" }} class="flex justify-center items-center">
+      <span class="loading loading-spinner" />
+    </div>
+  );
+}
+
 export const loader = async (
   props: Props,
   req: Request,
