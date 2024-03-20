f44a679e7aa496288cfe1c243296ba8ffe45002b
soutofernando <fernandosoutol45@gmail.com>
2024-03-20 17:29:23 -0300

    Infinite loop in carousel

--- components/ui/BannerCarousel.tsx
+++ components/ui/BannerCarousel.tsx
@@ -1,11 +1,9 @@
-import Button from "$store/components/ui/Button.tsx";
 import Icon from "$store/components/ui/Icon.tsx";
 import Slider from "$store/components/ui/Slider.tsx";
 import SliderJS from "$store/islands/SliderJS.tsx";
 import { useId } from "$store/sdk/useId.ts";
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import { Picture, Source } from "apps/website/components/Picture.tsx";
-import CarouselJS from "$store/islands/CarouselJS.tsx"
 
 /**
  * @titleBy alt
@@ -223,12 +221,12 @@ function BannerCarousel(props: Props) {
         )
         : null}
 
-      <CarouselJS
+      <SliderJS
         rootId={id}
         interval={autoplay?.interval && images && images.length > 1
           ? autoplay.interval * 1e3
           : undefined}
-        direction="horizontal"
+        infinite
       />
     </div>
   );
