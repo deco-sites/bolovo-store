f1da3c111e8ad7f8f611779d6ecfb461b55690d0
soutofernando <fernandosoutol45@gmail.com>
2024-04-17 23:01:10 -0300

    Fix image in productCard

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -334,8 +334,8 @@ function ProductCard(
                     media="(min-width: 1024px)"
                     fetchPriority={"low"}
                     src={safeSrc(back?.url ?? front.url)}
-                    width={239}
-                    height={300}
+                    width={310}
+                    height={310}
                   />
                   <img
                     class="h-full bg-base-100 col-span-full row-span-full w-full"
