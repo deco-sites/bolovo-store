822c3c49159b2341fd64282c963fce2b8e5852ba
Luigi <luigidias@protonmail.com>
2024-06-23 14:59:59 -0300

    fix: Reduce cls a little

--- sections/Category/CategoryMenu.tsx
+++ sections/Category/CategoryMenu.tsx
@@ -2,7 +2,7 @@ export { default, loader } from "../../components/search/CategoryMenu.tsx";
 
 export function LoadingFallback() {
   return (
-    <div class="flex mb-5 flex-col lg:flex-row justify-between lg:w-full flex-wrap pt-6 px-4 gap-6">
+    <div class="flex mb-5 flex-col lg:flex-row justify-between lg:w-full flex-wrap py-6 px-4 gap-6">
       <div class="flex items-center gap-4">
         <div class="skeleton h-6 w-[150px]" />
         <div class="text-black">&gt;</div>
