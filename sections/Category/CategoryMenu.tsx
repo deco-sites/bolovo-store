9749eeeeef5719fc2e59974acb4ab6dd7d8a2716
Luigi <luigidias@protonmail.com>
2024-06-23 14:52:30 -0300

    feat: Add PLP loading fallback

--- sections/Category/CategoryMenu.tsx
+++ sections/Category/CategoryMenu.tsx
@@ -2,8 +2,17 @@ export { default, loader } from "../../components/search/CategoryMenu.tsx";
 
 export function LoadingFallback() {
   return (
-    <div style={{ height: "716px" }} class="flex justify-center items-center">
-      <span class="loading loading-spinner" />
+    <div class="flex mb-5 flex-col lg:flex-row justify-between lg:w-full flex-wrap pt-6 px-4 gap-6">
+      <div class="flex items-center gap-4">
+        <div class="skeleton h-6 w-[150px]" />
+        <div class="text-black">&gt;</div>
+        <div class="skeleton h-6 w-[120px] rounded-full" />
+      </div>
+
+      <div class="flex items-center gap-8 justify-end">
+        <div class="skeleton w-20 h-5" />
+        <div class="skeleton w-20 h-5" />
+      </div>
     </div>
   );
 }
