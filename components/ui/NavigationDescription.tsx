ca43aa75f7de323500de2b46f79110af2783dc87
soutofernando <fernandosoutol45@gmail.com>
2024-07-10 23:39:59 -0300

    fix: navigationDescription layout

--- components/ui/NavigationDescription.tsx
+++ components/ui/NavigationDescription.tsx
@@ -31,9 +31,9 @@ export default function NavigationDescription(
         />
       )}
       <ul class="w-full flex flex-row lg:justify-start lg:gap-6 justify-between pb-3 pt-6">
-        {descriptionTabs?.slice(0, 3).map((tab, index) => (
+        {descriptionTabs?.filter((tab) => tab !== undefined).slice(0, 3).map((tab, index) => (
           <li
-            class="text-sm text-center flex flex-col justify-between items-center w-min h-[50px] cursor-pointer"
+            class={`text-sm text-center ${tab ? "flex" : "hidden"} flex-col justify-between items-center w-min h-[50px] cursor-pointer`}
             onClick={() => toggleDescription(index)}
           >
             <span>{tab?.title}</span>
@@ -46,7 +46,10 @@ export default function NavigationDescription(
         ))}
       </ul>
       <ul class="w-full flex flex-row">
-        {descriptionTabs?.slice(0, 3).map((description, index) => (
+        {descriptionTabs?.filter((tab) => tab !== undefined).slice(0, 3).map((
+          description,
+          index,
+        ) => (
           <li
             class={` ${
               itemVisible.value == index ? "flex" : "hidden"
