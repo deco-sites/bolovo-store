caf1833d8fc5874855af80ca5177168e0e869b77
soutofernando <fernandosoutol45@gmail.com>
2024-09-09 01:52:42 -0300

    feat: add notifyme feedback

--- components/product/OutOfStock.tsx
+++ components/product/OutOfStock.tsx
@@ -2,6 +2,7 @@ import { useSignal } from "@preact/signals";
 import { invoke } from "$store/runtime.ts";
 import type { Product } from "apps/commerce/types.ts";
 import type { JSX } from "preact";
+import { EMAIL_REGEX } from "deco-sites/bolovo-store/sdk/regex.ts";
 
 export interface Props {
   productID: Product["productID"];
@@ -10,6 +11,8 @@ export interface Props {
 
 function Notify({ productID, priceIntl = false }: Props) {
   const loading = useSignal(false);
+  const notifySuccess = useSignal(false);
+  const notifyInvalid = useSignal(false);
 
   const handleSubmit: JSX.GenericEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
@@ -19,40 +22,73 @@ function Notify({ productID, priceIntl = false }: Props) {
       const email =
         (e.currentTarget.elements.namedItem("email") as RadioNodeList)?.value;
 
+      if ((!email || !EMAIL_REGEX.test(email))) {
+        notifyInvalid.value = true;
+        return;
+      }
+
       await invoke.vnda.actions.notifyme({
         sku: productID,
         key: "bolovo-avise-me",
         email,
       });
+      notifySuccess.value = true;
+    } catch (error) {
+      notifyInvalid.value = true;
     } finally {
       loading.value = false;
     }
   };
 
   return (
-    <form class="form-control justify-start gap-2" onSubmit={handleSubmit}>
-      <span class="text-base">
-        {priceIntl
-          ? "This product is currently unavailable."
-          : "Este produto está indisponivel no momento"}
-      </span>
-      <span class="text-sm">
-        {priceIntl
-          ? "Notify me when it becomes available."
-          : "Avise-me quando estiver disponivel"}
-      </span>
+    <div>
+      {notifySuccess.value && (
+        <div class="flex flex-col justify-center">
+          <span class="text-lg font-semibold">
+            {priceIntl ? "Thank you!" : "Obrigado!"}
+          </span>
+          <p class="text-base font-medium mt-2">
+            {priceIntl
+              ? "Email successfully registered."
+              : "Email cadastrado com sucesso."}
+          </p>
+        </div>
+      )}
+      {!notifySuccess.value && (
+        <form class="form-control justify-start gap-2" onSubmit={handleSubmit}>
+          <span class="text-base">
+            {priceIntl
+              ? "This product is currently unavailable."
+              : "Este produto está indisponivel no momento"}
+          </span>
+          <span class="text-sm">
+            {priceIntl
+              ? "Notify me when it becomes available."
+              : "Avise-me quando estiver disponivel"}
+          </span>
 
-      <input
-        placeholder={priceIntl ? "Name" : "Nome"}
-        class="input input-bordered"
-        name="name"
-      />
-      <input placeholder="Email" class="input input-bordered" name="email" />
+          <input
+            placeholder={priceIntl ? "Name" : "Nome"}
+            class="input input-bordered"
+            name="name"
+          />
+          <input
+            placeholder="Email"
+            class="input input-bordered"
+            name="email"
+          />
 
-      <button class="btn disabled:loading" disabled={loading}>
-        {priceIntl ? "Send" : "Enviar"}
-      </button>
-    </form>
+          <button class="btn disabled:loading" disabled={loading}>
+            {priceIntl ? "Send" : "Enviar"}
+          </button>
+          {notifyInvalid.value && (
+            <span class="text-red-500">
+              {priceIntl ? "Invalid email" : "Email inválido"}
+            </span>
+          )}
+        </form>
+      )}
+    </div>
   );
 }
 
